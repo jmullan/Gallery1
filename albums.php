e487c0ca42d27c0d50106bfcf9e4e831e7848208
Bharat Mediratta <bharat@menalto.com>
2000-07-05 09:13:40 +0000

    Now handles the case where the app has not yet been configured and Does the Right Thing.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@14 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- albums.php
+++ albums.php
@@ -18,8 +18,34 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 ?>
+<? if (file_exists("config.php")) { ?>
 <title> The Photo Gallery </title>
 <frameset rows="*,10" border=0 frameborder="no">
  <frame src="albums_content.php" name=content frameborder=1>
  <frame src="albums_description.php" name=description frameborder=1>
 </frameset>
+<?
+} else {
+	if (file_exists("setup") && is_readable("setup")) {
+		header("Location: setup");
+		return;
+	}
+
+	require("style.php");
+?>
+<center>
+<font size=+2>Gallery has not been configured!</font>
+<p>
+Your installation of Gallery has not yet been configured.
+To configure it, type:
+	<table><tr><td>
+		<code>
+		% cd <?=dirname(getenv("SCRIPT_FILENAME"))?>
+		<br>
+		% sh ./configure.sh
+	</td></tr></table>
+<p>
+And then go <a href=setup>here</a>
+<?
+} 
+?>
