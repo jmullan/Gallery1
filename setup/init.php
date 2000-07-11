6244a60b15739e6bffb2feed471b9fa2d910c7c8
Bharat Mediratta <bharat@menalto.com>
2000-07-11 03:02:08 +0000

    Pushed globals into this file for convenience.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@49 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- setup/init.php
+++ setup/init.php
@@ -1,5 +1,10 @@
 <?
 /*
- * Init prepend file for setup directory, currently empty.
+ * Init prepend file for setup directory.
  */
+
+$GALLERY_DIR = dirname(dirname(getenv("SCRIPT_FILENAME")));
+$GALLERY_URL = dirname(dirname(getenv("SCRIPT_NAME")));
+$MIN_PHP_MAJOR_VERSION = 4;
+
 ?>
