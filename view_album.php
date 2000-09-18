ffbc0c80204f010545f20403cc4d80a66ec080f9
Chris Smith <jackodog@gmail.com>
2000-09-18 06:00:35 +0000

    Added .php extension to do_command urls that were mysteriously missing them.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@107 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- view_album.php
+++ view_album.php
@@ -198,9 +198,9 @@ if ($numPhotos) {
 				echo(popup("highlight_photo.php?index=$i"));
 				echo(">[highlight]</a>");
 				if ($album->isHidden($i)) {
-					echo("<a href=do_command?cmd=show&index=$i&return=view_album.php>[show]</a>");
+					echo("<a href=do_command.php?cmd=show&index=$i&return=view_album.php>[show]</a>");
 				} else {
-					echo("<a href=do_command?cmd=hide&index=$i&return=view_album.php>[hide]</a>");
+					echo("<a href=do_command.php?cmd=hide&index=$i&return=view_album.php>[hide]</a>");
 				}
 			}
 			echo("<hr size=1>");
@@ -272,9 +272,9 @@ Admin:
 <a href=<?= popup("add_photos.php?albumName=$albumName") ?>>[Add Photos] </a>
 <a href=<?= popup("shuffle_album.php?albumName=$albumName") ?>>[Shuffle Photos] </a>
 <a href=<?= popup("resize_photo.php?albumName=$albumName&index=all") ?>>[Resize All] </a>
-<a href=<?= popup("do_command?cmd=remake-thumbnail&albumName=$albumName&index=all") ?>>[Rebuild Thumbs]</a>
+<a href=<?= popup("do_command.php?cmd=remake-thumbnail&albumName=$albumName&index=all") ?>>[Rebuild Thumbs]</a>
 <a href=<?= popup("edit_appearance.php?albumName=$albumName") ?>>[Edit Appearance] </a>
-<a href=do_command?cmd=leave-edit&return=view_album.php>[Leave edit mode]</a>
+<a href=do_command.php?cmd=leave-edit&return=view_album.php>[Leave edit mode]</a>
 </font>
 <? }  else { ?>
 <a href=<?= popup("edit_mode.php")?>>[Enter edit mode]</a>
