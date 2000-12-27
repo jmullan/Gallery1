9495f0bc21ca6017acbcf6048ba1e29cd2136306
Bharat Mediratta <bharat@menalto.com>
2000-12-27 02:39:01 +0000

    Removed a line of debug code (bad jackodog, no biscuit)
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@157 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- edit_caption.php
+++ edit_caption.php
@@ -18,7 +18,6 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 ?>
-data = <?= $data ?><br>
 <?
 if ($save) {
 	$album->setCaption($index, stripslashes($data));
