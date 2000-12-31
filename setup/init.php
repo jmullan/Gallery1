025f7ee4f8a1c26491db015279fe27c9394c786c
Bharat Mediratta <bharat@menalto.com>
2000-12-31 22:03:58 +0000

    Older versions of mod_rewrite require that you escape the & symbol in URLs.  Newer versions require that you DON'T escape it.  Aiee!
    In setup's .htaccess we attempt to use mod_rewrite.  The rewritten URL
    is crafted so that if mod_rewrite requires the & to be escaped, the
    init_mod_rewrite variable contains an extra "ampersandbroken" string
    which we can detect in PHP.  This allows us to create the top level
    .htaccess file appropriately.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@196 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- setup/init.php
+++ setup/init.php
@@ -7,4 +7,15 @@ $GALLERY_DIR = dirname(dirname(getenv("SCRIPT_FILENAME")));
 $GALLERY_URL = dirname(dirname(getenv("SCRIPT_NAME")));
 $MIN_PHP_MAJOR_VERSION = 4;
 
+if ($init_mod_rewrite) {
+	$GALLERY_REWRITE = 1;
+	if (strstr($init_mod_rewrite, "ampersandbroken")) {
+		$GALLERY_REWRITE_SEPARATOR = "\&";
+	} else {
+		$GALLERY_REWRITE_SEPARATOR = "&";
+	}
+} else {
+	$GALLERY_REWRITE = 0;
+}
+
 ?>
