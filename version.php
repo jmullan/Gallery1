7d2df2c306f0c99ec5c1d2661c8ee0340a63ec03
Bharat Mediratta <bharat@menalto.com>
2001-01-02 08:45:17 +0000

    Switched the home URL to gallery.sourceforge.net
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@226 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <? 
 $gallery->version = "1.0 beta 5";
 $gallery->config_version = 3;
-$gallery->url = "http://www.sourceforge.com/projects/gallery";
+$gallery->url = "http://gallery.sourceforge.net";
 ?>
