4bd17c4b407935c5b234e74d17ece3b4a1699b50
Chris Smith <jackodog@gmail.com>
2001-01-05 07:59:46 +0000

    Rev'd version because we introduced galleryTitle to the app
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@245 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <? 
-$gallery->version = "1.0 beta 5";
-$gallery->config_version = 3;
+$gallery->version = "1.0 beta 6";
+$gallery->config_version = 4;
 $gallery->url = "http://gallery.sourceforge.net";
 ?>
