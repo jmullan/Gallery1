9b1d4bc0dfad25f798a684b7cbe3459adcaa387e
Bharat Mediratta <bharat@menalto.com>
2001-02-15 10:00:35 +0000

    Make sure GALLERY_URL doesn't end in a slash
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@477 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- setup/init.php
+++ setup/init.php
@@ -5,6 +5,10 @@
 
 $GALLERY_DIR = dirname(dirname(getenv("SCRIPT_FILENAME")));
 $GALLERY_URL = dirname(dirname(getenv("SCRIPT_NAME")));
+
+// Make sure GALLERY_URL doesn't end in a slash
+$GALLERY_URL = preg_replace("/\/$/", "", $GALLERY_URL);
+
 $MIN_PHP_MAJOR_VERSION = 4;
 
 if ($init_mod_rewrite) {
