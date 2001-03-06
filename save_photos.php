a96f7d05e1f13085cb020dcefb4993a60abe5197
dpk <dpk@users.sourceforge.net>
2001-03-06 08:15:19 +0000

    HTML Slurpage. Use urls[] instead of url. Process any recognizable filetypes and slurp all others for img and href links to images/movies. After all other processing we list found links in a checkbox form for additional processing. This form just submits the links as urls[] back into save_photos.php.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@533 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- save_photos.php
+++ save_photos.php
@@ -52,16 +52,37 @@ function msg($buf) {
 </head>
 <body onLoad='opener.location.reload(); '>
 
+<span class=title>Fetching Urls...</span>
+<br>
+
 <?
-if ($url) {
-	$file = basename($url);
+/* Process all urls first */
+$temp_files = array();
+foreach ($urls as $url) {
+
+	/* Get rid of any preceding whitespace (fix for odd browsers like konqueror) */
+	$url = eregi_replace("^[[:space:]]+", "", $url);
+
+	/* Parse URL for name and file type */
+	$url_stuff = parse_url($url);
+	$name = basename($url_stuff["path"]);
 	$tag = ereg_replace(".*\.([^\.]*)$", "\\1", $url);
 	$tag = strtolower($tag);
 
+	/* Dont output warning messages if we cant open url */
+	$id = @fopen($url, "r");
+
+	/* Manual check - was the url accessible? */
+	if (!$id) {
+		msg("Could not open url: '$url'");
+		continue;
+	} else {
+		msg("$url");
+	}
+
 	/* copy file locally */
-	$urlFile = "$app->tmpDir/photo.$file";
-	$id = fopen($url, "r");
-	$od = fopen($urlFile, "w");
+	$file = "$app->tmpDir/photo.$name";
+	$od = fopen($file, "w");
 	if ($id && $od) {
 		while (!feof($id)) {
 			fwrite($od, fread($id, 65536));
@@ -70,17 +91,80 @@ if ($url) {
 		fclose($id);
 		fclose($od);
 	}
+	/* Make sure we delete this file when we're through... */
+	$temp_files[] = $file;
 
-	/* Tack it onto userfile */
-	$userfile_name[] = $file;
-	$userfile[] = $urlFile;
+	/* If this is an image or movie - add it to the processor array */
+	if (	!strcmp($tag, "jpg") ||
+		!strcmp($tag, "gif") ||
+		!strcmp($tag, "png") ||
+		!strcmp($tag, "avi") ||
+		!strcmp($tag, "mpg") ||
+		!strcmp($tag, "wmv") ||
+		!strcmp($tag, "zip")	) {
+
+		/* Tack it onto userfile */
+		$userfile_name[] = $name;
+		$userfile[] = $file;
+
+	} else {
+		/* Slurp the file */
+		msg("Parsing $url for images...");
+		$fd = fopen ($file, "r");
+		$contents = fread ($fd, filesize ($file));
+		fclose ($fd);
+
+		/* We'll need to add some stuff to relative links */
+		$base_url = $url_stuff["scheme"] . '://' . $url_stuff["host"];
+		$base_dir = '';
+		if ($url_stuff["port"]) {
+		  $base_url .= ':' . $url_stuff["port"];
+		}
+
+		/* Hack to account for broken dirname */
+		if (ereg("/$", $url_stuff["path"])) {
+			$base_dir = $url_stuff["path"];
+		} else {
+			$base_dir = dirname($url_stuff["path"]);
+		}
+
+		/* Make sure base_dir ends in a / ( accounts for empty base_dir ) */
+		if (!ereg("/$", $base_dir)) {
+			$base_dir .= '/';
+		}
+
+		/* Perl Regex: Find all src= and href= links to valid file types */
+		if(preg_match_all ('/(src|href)="?([^" >]+\.(jpg|gif|png|mpg|avi|wmv))[" >]/is', $contents, $things, PREG_PATTERN_ORDER)) {
+
+			/* Add each unique link to an array we scan later */
+			foreach (array_unique($things[2]) as $thing) {
+
+				/* Absolute Link ( http://www.foo.com/bar ) */
+				if (substr($thing, 0, 4) == 'http') {
+					$image_tags[] = $thing;
+
+				/* Relative link to the host ( /foo.bar )*/
+				} elseif (substr($thing, 0, 1) == '/') {
+					$image_tags[] = $base_url . $thing;
+
+				/* Relative link to the dir ( foo.bar ) */
+				} else {
+					$image_tags[] = $base_url . $base_dir . $thing;
+				}
+			}
+		}
+
+		/* Tell user how many links we found, but delay processing */
+		msg("Found " . count($image_tags) . " Images.");
+	}
 }
-
 ?>
+
+<br>
 <span class=title>Processing status...</span>
 <br>
-<?
 
+<?
 while (sizeof($userfile)) {
 	$name = array_shift($userfile_name);
 	$file = array_shift($userfile);
@@ -117,8 +201,8 @@ while (sizeof($userfile)) {
 }
 
 /* Clean up the temporary url file */
-if ($urlFile) {
-	unlink($urlFile);
+foreach ($temp_files as $tf) {
+	unlink($tf);
 }
 
 function process($file, $tag, $name) {
@@ -164,5 +248,21 @@ if (!$msgcount) {
 <form>
 <input type=submit value="Dismiss" onclick='parent.close()'>
 </form>
+<?
+/* Prompt for additional files if we found links in the HTML slurpage */
+if (count($image_tags)) {
+?>
+<form enctype="multipart/form-data" action="save_photos.php" method=post name="uploadurl_form">
+<table border=0>
+<?
+	/* Allow user to select which files to grab - only show url right now ( no image previews ) */
+	foreach ( $image_tags as $image_src) {
+		print "<tr><td><input type=checkbox name=\"urls[]\" value=\"$image_src\" checked>$image_src</td></tr>\n";
+	}
+?>
+</table>
+<input type=submit value="Add Files" onClick="opener.showProgress(); document.uploadurl_form.submit()">
+</form>
+<? } /* End if links slurped */ ?>
 </body>
 </html>
