0e052a56ff826c0927122baebf4eb409784d209b
Bharat Mediratta <bharat@menalto.com>
2001-03-25 21:53:45 +0000

    Added extra fallback code to use HTTP_ENV_VARS and then SCRIPT_XXX in case HTTP_SERVER_VARS isn't defined.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/RELEASE_1_0@635 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/setup/init.php
+++ gallery/setup/init.php
@@ -2,9 +2,24 @@
 /*
  * Init prepend file for setup directory.
  */
+$tmp = $HTTP_SERVER_VARS["PATH_TRANSLATED"];
+if (!$tmp) {
+	$tmp = $HTTP_ENV_VARS["PATH_TRANSLATED"];
+}
+if (!$tmp) {
+	$tmp = getenv("SCRIPT_FILENAME");
+}
+$GALLERY_DIR = dirname(dirname($tmp));
 
-$GALLERY_DIR = dirname(dirname($HTTP_SERVER_VARS["PATH_TRANSLATED"]));
-$GALLERY_URL = dirname(dirname($HTTP_SERVER_VARS["PHP_SELF"]));
+$tmp = $HTTP_SERVER_VARS["PHP_SELF"];
+if (!$tmp) {
+	$tmp = $HTTP_ENV_VARS["PHP_SELF"];
+}
+if (!$tmp) {
+	$tmp = getenv("SCRIPT_NAME");
+}
+
+$GALLERY_URL = dirname(dirname($tmp));
 
 // Make sure GALLERY_URL doesn't end in a slash
 $GALLERY_URL = preg_replace("/\/$/", "", $GALLERY_URL);
