b0d962d2bedf3e6624e2919ff6d4dd115cbc2c51
Bharat Mediratta <bharat@menalto.com>
2001-03-25 22:09:17 +0000

    Merged changes from RELEASE_1_0
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@638 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- errors/configure_instructions.php
+++ errors/configure_instructions.php
@@ -1,13 +1,23 @@
 <?
 function configure($cmd="configure.sh") {
-	global $HTTP_SERVER_VARS;
+	global $HTTP_SERVER_VARS, $HTTP_ENV_VARS;
+
+	$tmp = $HTTP_SERVER_VARS["PATH_TRANSLATED"];
+	if (!$tmp) {
+		$tmp = $HTTP_ENV_VARS["PATH_TRANSLATED"];
+	}
+	if (!$tmp) {
+		$tmp = getenv("SCRIPT_FILENAME");
+	}
+	$path = dirname(dirname($tmp));
+
 ?>
 <center>
  <table>
   <tr>
    <td>
     <code>
-     <br> % cd <?=dirname($HTTP_SERVER_VARS["PATH_TRANSLATED"])?>
+     <br> % cd <?= $path ?>
      <br> % sh ./<?=$cmd?>
    </td>
   </tr>
