61069729b10f39bba19341d3d01000f96ff99875
Chris Smith <jackodog@gmail.com>
2001-05-29 18:05:02 +0000

    Handles nested albums [ part of the John Kirkland nested album patch].
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@706 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- rename_album.php
+++ rename_album.php
@@ -47,6 +47,30 @@ if ($newName) {
 	$newName = ereg_replace("\-+$", "", $newName);
 	$albumDB->renameAlbum($oldName, $newName);
 	$albumDB->save();
+	// need to account for nested albums by updating
+	// the parent album when renaming an album
+        if ($album->fields[parentAlbumName]) {
+		$parentName = $album->fields[parentAlbumName];
+		print "parentName=".$parentName."<br>";
+		print "newName=".$newName."<br>";
+		print "oldName=".$oldName."<br>";
+		$parentAlbum = $albumDB->getAlbumbyName($parentName);
+		for ($i=1; $i <= $parentAlbum->numPhotos(1); $i++) {
+			if ($parentAlbum->isAlbumName($i) == $oldName) {
+				$parentAlbum->setIsAlbumName($i,$newName);
+				$parentAlbum->save();
+				break;
+			}
+		}
+	}
+	// then we need to update the parentAlbumName field in the children
+	for ($i=1; $i <= $album->numPhotos(1); $i++) {
+		if ($album->isAlbumName($i)) {
+			$childAlbum = $album->getNestedAlbum($i);
+			$childAlbum->fields[parentAlbumName] = $newName;
+			$childAlbum->save();
+		}
+	}
 	dismissAndReload();
 	return;
 } else {
