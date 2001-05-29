e31b08a93ef1b52bcb24259dad04751ba2b78ff8
Chris Smith <jackodog@gmail.com>
2001-05-29 18:04:14 +0000

    Can now move a photo (or album), or a group of photos/albums around or to another album [ part of the John Kirkland nested album patch].
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@705 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- move_photo.php
+++ move_photo.php
@@ -24,6 +24,9 @@
 if (!$user->canWriteToAlbum($album)) {
 	exit;
 }
+
+$albumDB = new AlbumDB(); // read album database
+
 ?>
 <html>
 <head>
@@ -35,7 +38,93 @@ if (!$user->canWriteToAlbum($album)) {
 <?
 if ($albumName && isset($index)) {
 	$numPhotos = $album->numPhotos(1);
-	if (isset($newIndex)) {
+
+	// Here we are "moving" a photo from one album to another by "adding" it to the new album
+	// and then deleting it from the old one.  This could be optimized because our thumnails 
+	// and resized images already exist in the original directory, but the current method is an easy
+	// way to make sure all thumbnails and resized images are the correct size.
+
+        if (isset($newAlbum)) {	// we are moving from one album to another
+            	$postAlbum = $albumDB->getAlbumbyName($newAlbum);
+	    	if ($album != $postAlbum) {
+			//$startPhoto=$index;
+			//$endPhoto=$startPhoto+max($numPhotosToMove,1);
+
+			//if ($startPhoto > $endPhoto) { // the end photo value needs to be greater than the start value
+			//	dismissAndReload();
+			//	return;
+			//}
+			
+			if ($album->isAlbumName($index)) { // moving "album" to another location
+				if ($newAlbum == "ROOT") { // moving "album" to ROOT location
+					$myAlbum = $album->getNestedAlbum($index);
+					$myAlbum->fields[parentAlbumName] = 0;
+					$album->deletePhoto($index); 
+					$myAlbum->save();
+					$album->save();
+				} else { // moving "album" to another album
+					$myAlbum = $album->getNestedAlbum($index);
+					if ($postAlbum != $myAlbum) { // we don't ever want to point an album back at itself!!!
+						$postAlbum->addNestedAlbum($album->isAlbumName($index)); // copy "album" to new album
+						$myAlbum->fields[parentAlbumName] = $postAlbum->fields[name];
+						$album->deletePhoto($index); // delete "album" from original album
+						$postAlbum->save();
+						$album->save();
+						$myAlbum->save();
+					}
+				}
+			} else { // moving "picture" to another album
+				$index = $startPhoto; // set the index to the first photo that we are moving.	
+				while ($startPhoto <= $endPhoto) {
+					if (!$album->isAlbumName($index)) {
+						print "<br>Moving photo #".$startPhoto."<br>";
+						my_flush();
+						$mydir = $album->getAlbumDir();
+						$myphoto = $album->getPhoto($index);
+						$myname = $myphoto->image->name;
+						$mytype=$myphoto->image->type;
+						$myfile="$mydir/$myname.$mytype";
+						//print "mydir=".$mydir."<br>";
+						//print "myphoto=".$myphoto."<br>";
+						//print "myname=".$myname."<br>";
+						//print "mytype=".$mytype."<br>";
+						//print "myfile=".$myfile."<br>";
+						$err = $postAlbum->addPhoto($myfile, $mytype);
+						if (!$err) {
+							$newPhotoIndex = $postAlbum->numPhotos(1);
+							// Set the caption of the new photo
+							$postAlbum->setCaption($newPhotoIndex, $album->getCaption($index));
+							$postAlbum->save();
+							/* resize the photo if needed */
+							if ($postAlbum->fields["resize_size"] > 0 ) {
+								$photo = $postAlbum->getPhoto($newPhotoIndex);
+								list($w, $h) = $photo->getDimensions();
+								if ($w > $postAlbum->fields["resize_size"] ||
+								    $h > $postAlbum->fields["resize_size"]) {
+									print "- Resizing #".$startPhoto;
+									$postAlbum->resizePhoto($newPhotoIndex, $postAlbum->fields["resize_size"]);
+								}
+							}
+							$postAlbum->save();
+							$album->deletePhoto($index);
+							$album->save();
+						} else {
+							print "<font color=red>Error: $err!</font>";
+							return;
+                				}
+			     		} else {
+						print "<br>Skipping Album #".$startPhoto."<br>";
+						$index++; // we hit an album... don't move it... just increment the index
+					}
+					$startPhoto++;
+	    			} //end while
+			} //end else
+		} //end if ($album != $postAlbum)
+		dismissAndReload();
+		return;
+	} //end if (isset($newAlbum))
+
+        if (isset($newIndex)) {
 		$album->movePhoto($index, $newIndex);
 		$album->save();
 		dismissAndReload();
@@ -44,31 +133,100 @@ if ($albumName && isset($index)) {
 ?>
 
 <center>
-<font size=+0>
-Select the new location of photo #<?=$index?>:
+Move this photo/album within this album:<br>
+<i>(Current Location is <?=$index?>)</i>
+<p>
+<?= $album->getThumbnailTag($index) ?>
+<p>
 <form name="theform">
+Select the new location:
 <input type=hidden name="index" value="<?=$index?>">
 <select name="newIndex">
 <?
 for ($i = 1; $i <= $numPhotos; $i++) {
-	$sel = "";
-	if ($i == $index+1) {
-		$sel = "selected";
-	} 
-	$j = $i - 1;
-	echo "<option value=$j $sel> $i</option>";
+        $sel = "";
+        if ($i == $index) {
+                $sel = "selected";
+        }
+		$j = $i - 1;
+        echo "<option value=j $sel> $i</option>";
 }
 ?>
 </select>
+<p>
 <input type=submit value="Move it!">
 <input type=submit name="submit" value="Cancel" onclick='parent.close()'>
 </form>
-
 <p>
-<?= $album->getThumbnailTag($index) ?>
-
+<hr size=1>
+<b>OR</b>
+<hr size=1>
+Move a range of photos/albums to a new album:<br>
+<i>(To move just one photo/album, make First and Last the same)</i><p>
+<form name=move_to_album_form>
+<input type=hidden name="index" value="<?=$index?>">
 <?
-	}
+if ($album->isAlbumName($index)) {
+?>
+<select name="newAlbum">
+<?
+	printAlbumOptionList(1,0,0);
+} else {  
+// Display album list for a photo and display num photos to move
+?>
+<table>
+<tr>
+<td align=center><b>First</b></td>
+<td align=center><b>Last</b></td>
+<td align=center><b>New Album</b></td>
+</tr>
+<tr>
+<td align=center>
+<select name="startPhoto">
+<option value=1 selected> 1</option>
+<?
+for ($i = 2; $i <= $numPhotos; $i++) {
+        $sel = "";
+        if ($i == $index) {
+                $sel = "selected";
+        }
+        echo "<option value=$i $sel> $i</option>";
+}
+?>
+</select>
+</td>
+<td align=center>
+<select name="endPhoto">
+<option value=1 selected> 1</option>
+<?
+for ($i = 2; $i <= $numPhotos; $i++) {
+        $sel = "";
+        if ($i == $index) {
+                $sel = "selected";
+        }
+        echo "<option value=$i $sel> $i</option>";
+}
+?>
+</select>
+</td>
+<td>
+<select name="newAlbum">
+<?
+printAlbumOptionList(0,0,1); 
+?>
+</td>
+</tr>
+</table>
+<?
+} // end else
+?>
+</select>
+<br>
+<input type=submit value="Move to Album!">
+<input type=submit name="submit" value="Cancel" onclick='parent.close()'>
+</form>
+<?
+}
 } else {
 	error("no album / index specified");
 }
