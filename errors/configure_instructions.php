31464c49d7f6a92e79944f3be7b92500470f1c67
Bharat Mediratta <bharat@menalto.com>
2001-07-03 21:58:55 +0000

    Removed the code that tried to figure out the filesystem path to your gallery because too often it was wrong and that caused confusion.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@822 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- errors/configure_instructions.php
+++ errors/configure_instructions.php
@@ -1,23 +1,12 @@
 <?
 function configure($cmd="configure.sh") {
-	global $HTTP_SERVER_VARS, $HTTP_ENV_VARS;
-
-	$tmp = $HTTP_SERVER_VARS["PATH_TRANSLATED"];
-	if (!$tmp) {
-		$tmp = $HTTP_ENV_VARS["PATH_TRANSLATED"];
-	}
-	if (!$tmp) {
-		$tmp = getenv("SCRIPT_FILENAME");
-	}
-	$path = dirname(dirname($tmp));
-
 ?>
 <center>
  <table>
   <tr>
    <td>
     <code>
-     <br> % cd <?= $path ?>
+     <br> % cd /path/to/your/gallery
      <br> % sh ./<?=$cmd?>
    </td>
   </tr>
