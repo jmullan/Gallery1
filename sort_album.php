f8bcf9ca98288e1802334b513194fd2f437eb9e2
John Kirkland <jpk@bl.org>
2001-07-19 06:30:06 +0000

    Required for sorting
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@888 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- sort_album.php
+++ sort_album.php
@@ -27,7 +27,7 @@ if (!$gallery->user->canWriteToAlbum($gallery->album)) {
 ?>
 <html>
 <head>
-  <title>Shuffle Album</title>
+  <title>Sort Album</title>
   <?= getStyleSheetLink() ?>
 </head>
 <body>
@@ -35,20 +35,25 @@ if (!$gallery->user->canWriteToAlbum($gallery->album)) {
 <?
 if ($gallery->session->albumName) {
 	if ($confirm) {
-		$gallery->album->shufflePhotos();
-		$gallery->album->save();
-		dismissAndReload();
-		return;
+		if (!strcmp($sort,"random")) {
+			$gallery->album->shufflePhotos();
+			$gallery->album->save();
+			dismissAndReload();
+			return;
+		} else {
+			$gallery->album->sortPhotos($sort,$order);
+			$gallery->album->save();
+			dismissAndReload();
+			return;
+		}
 	} else {
 ?>
 
 <center>
-Do you really want to shuffle all the photos in this album?  This operation can't be undone.
+Select your sorting criteria for this album below?
 <br>
-<form>
-<input type=submit name=confirm value="Yes">
-<input type=submit value="Cancel" onclick='parent.close()'>
-</form>
+Warning:  This operation can't be undone.
+<br>
 <br>
 
 <p>
@@ -60,6 +65,28 @@ if ($gallery->album->getHighlight()) {
 <br>
 <?= $gallery->album->fields["caption"] ?>
 
+<form>
+<table>
+  <tr>
+    <td><input checked type="radio" name="sort" value="upload">&nbsp;By Upload Date</td>
+  </tr>
+  <tr>
+    <td><input type="radio" name="sort" value="itemCapture">&nbsp;By Picture-Taken Date</td>
+  </tr>
+  <tr>
+    <td><input type="radio" name="sort" value="random">&nbsp;Randomly</td>
+  </tr>
+  <tr>
+<select name="order">
+  <option value="0">Ascending</option>
+  <option value="1">Descending</option>
+</select>
+  </tr>
+</table>
+<br>
+<input type=submit name=confirm value="Sort">
+<input type=submit value="Cancel" onclick='parent.close()'>
+</form>
 <?
 	}
 } else {
