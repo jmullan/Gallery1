f6a55371802d37699f558864736679d5664a64f9
Bharat Mediratta <bharat@menalto.com>
2001-08-02 06:08:33 +0000

    Bumped config_version to 23, album_version to 2
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1023 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -10,7 +10,7 @@ if (!$gallery) {
 }
 
 $gallery->version = "1.2-cvs";
-$gallery->config_version = 22;
-$gallery->album_version = 1;
+$gallery->config_version = 23;
+$gallery->album_version = 2;
 $gallery->url = "http://gallery.sourceforge.net";
 ?>
