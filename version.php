7b2de73a8392a74a321bc6989288d03b3334a88b
Bharat Mediratta <bharat@menalto.com>
2001-08-03 21:43:10 +0000

    Arbitrarily changed CVS version to: $gallery->version = "1.2-cvs-b1";
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1068 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -9,7 +9,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2-cvs";
+$gallery->version = "1.2-cvs-b1";
 $gallery->config_version = 23;
 $gallery->album_version = 3;
 $gallery->url = "http://gallery.sourceforge.net";
