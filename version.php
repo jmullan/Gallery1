c24d7b38e1a260dc85d81cd618f4e3f7078f1d30
Bharat Mediratta <bharat@menalto.com>
2001-08-04 23:52:38 +0000

    2001-08-04  Bharat Mediratta  <bharat@menalto.com>  (1.2-cvs-b3)
            * Added a hack that allows you to do multi-level album highlighting.
              It's still a bit limited; if you change the highlight at the
              innermost level you still need to update it every step of the way
              back up the chain.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1078 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -9,7 +9,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2-cvs-b2";
+$gallery->version = "1.2-cvs-b3";
 $gallery->config_version = 23;
 $gallery->album_version = 3;
 $gallery->url = "http://gallery.sourceforge.net";
