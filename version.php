0a0b6b4fb5f42d6cec2d46e6d66e8702abf6ee9c
Bharat Mediratta <bharat@menalto.com>
2001-08-20 07:20:31 +0000

    2001-08-20  Bharat Mediratta  <bharat@menalto.com> (1.2-cvs-b16)
            * Fixed a minor bug that would cause index-out-of-bounds errors when
              trying to move albums.  The error is visible in Netscape, but not in
              IE.  The function would still work, but it would look bad.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1139 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -9,7 +9,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2-cvs-b15";
+$gallery->version = "1.2-cvs-b16";
 $gallery->config_version = 23;
 $gallery->album_version = 3;
 $gallery->url = "http://gallery.sourceforge.net";
