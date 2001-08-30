0f5cf72d31d14695d32b7840d973a3aa1a0de568
Bharat Mediratta <bharat@menalto.com>
2001-08-30 06:07:20 +0000

    * Potentially fixed a bug on PHP 4.0.1 where it complains that the           fs_XXX methods are redefined.  This is happening because the           require() method fires even when the line it's on never gets           executed.  Replace those with include().
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1155 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -9,7 +9,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2-cvs-b17";
+$gallery->version = "1.2-cvs-b18";
 $gallery->config_version = 23;
 $gallery->album_version = 3;
 $gallery->url = "http://gallery.sourceforge.net";
