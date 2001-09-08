ad0db965f465a6cb5ae1e52a78d28ab7c33513a1
Bharat Mediratta <bharat@menalto.com>
2001-09-08 20:04:47 +0000

    Added FTP instructions.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1173 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- errors/configure_instructions.php
+++ errors/configure_instructions.php
@@ -6,18 +6,40 @@ function configure($cmd="configure") {
   <tr>
    <td>
     <code>
-     <br> <b>Unix</b>
+     <br> <b>Unix</b> with shell access
      <br> % cd /path/to/your/gallery
      <br> % sh ./<?=$cmd?>.sh
+     <br>
+     <br> <b>Unix</b> with FTP access
+     <br> ftp> chmod <?= configure_filemode($cmd) ?> .htaccess
+     <br> ftp> chmod <?= configure_filemode($cmd) ?> config.php
+     <br> ftp> chmod <?= configure_dirmode($cmd) ?> setup
      <br>
      <br> <b>Windows</b>
      <br> C:\> cd \path\to\your\gallery
      <br> C:\> <?=$cmd?>.bat
+     <br>
      <br>
    </td>
   </tr>
  </table>
 </center>
 <?
+}
+
+function configure_filemode($cmd = "configure") {
+	if (!strcmp($cmd, "configure")) {
+		return 777;
+	} else {
+		return 644;
+	}
+}
+
+function configure_dirmode($cmd = "configure") {
+	if (!strcmp($cmd, "configure")) {
+		return 755;
+	} else {
+		return 0;
+	}
 }
 ?>
