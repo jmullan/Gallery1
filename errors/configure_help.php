5a2d560f93006411daaec4778e49ae030689e95e
Bharat Mediratta <bharat@menalto.com>
2001-09-08 20:04:19 +0000

    Brief instructions about what to do when there is a configuration problem.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1171 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ errors/configure_help.php
@@ -0,0 +1,5 @@
+If you experience 
+problems, you might find the solution in the README or 
+in the <a href=http://gallery.sourceforge.net/faq.php>
+FAQ</a>.  If not, you can ask for help on the <a href=http://sourceforge.net/mail/?group_id=7130>
+Gallery Users Mailing List</a>.
