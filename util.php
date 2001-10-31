4b1d0b4d074aa5481f2cd9734ffc6044bc1bce44
Bharat Mediratta <bharat@menalto.com>
2001-10-31 09:34:51 +0000

    Oops -- forgot to check this in as part of the b6 update. Check to see which ppmtojpeg/pnmtojpeg binary to use and use the right one.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1341 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- util.php
+++ util.php
@@ -411,7 +411,7 @@ function fromPnmCmd($file) {
 	if (eregi("\.png", $file)) {
 		$cmd = NetPBM("pnmtopng");
 	} else if (eregi("\.(jpg|jpeg)", $file)) {
-		$cmd = NetPBM("ppmtojpeg", "--quality=95");
+		$cmd = NetPBM($gallery->app->pnmtojpeg, "--quality=95");
 	} else if (eregi(".gif", $file)) {
 		$cmd = NetPBM("ppmquant", "256") . " | " . NetPBM("ppmtogif");
 	}
