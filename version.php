34703be0bc63dffbabec5e0d1184237f11934e42
Bharat Mediratta <bharat@menalto.com>
2001-11-18 06:10:03 +0000

    Added PostNuke compliant $modversion[] array.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1399 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- version.php
+++ version.php
@@ -43,4 +43,21 @@ $gallery->config_version = 26;
 $gallery->album_version = 4;
 $gallery->remote_protocol_version = 1;
 $gallery->url = "http://gallery.sourceforge.net";
+
+/*
+ * PostNuke version info
+ */
+$modversion['name'] = 'gallery'; // Module name
+$modversion['version'] = $gallery->version; // Version Number
+$modversion['description'] = 'Gallery is a slick web based photo album written using PHP. Easy to install (it includes a config wizard), it provides users with the ability to create and maintain their own albums in the album collection via an intuitive web interface. Photo management includes automatic thumbnail creation, image resizing, rotation, ordering, captioning, searching and more. Albums can have read, write and caption permissions per individual authenticated user for an additional level of privacy. '; // Module Description
+$modversion['credits'] = ''; // Credits File
+$modversion['help'] = 'README'; // Help File
+$modversion['changelog'] = 'ChangeLog'; //Change Log File
+$modversion['license'] = 'LICENSE.txt'; // License File
+$modversion['official'] = 1; // Official PostNuke Approved Module? 1 = yes, 0 = no
+$modversion['author'] = 'Bharat Mediratta'; // Author
+$modversion['contact'] = $gallery->url; // The Authors Website or Contact Email Address
+$modversion['admin'] = 0; // Leave at 0
 ?>
+
+
