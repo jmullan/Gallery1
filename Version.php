4ebec489631f3706219b78957c19b899ed44938c
Bharat Mediratta <bharat@menalto.com>
2001-12-05 04:41:35 +0000

    2001-12-04  Bharat Mediratta  <bharat@menalto.com> (1.2.5-cvs-b1)
            * README: added more info on how to do CHMOD operations from CuteFTP.
    
            * Incorporated a patch from Adam Lazur (adam@lazur.org) which increases
              Gallery's ability to deal with symbolic links.  It checks to see if a
              file is a symlink, and that the symlink is valid, before trying to
              open/read it.  This patch enables Adam's debian package of Gallery.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1481 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -38,7 +38,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2.4-p1-cvs-b1";
+$gallery->version = "1.2.5-cvs-b1";
 $gallery->config_version = 26;
 $gallery->album_version = 4;
 $gallery->remote_protocol_version = 1;
