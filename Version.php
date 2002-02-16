bef7b80f731e3cbd313560c0b7f16360f1c20d0a
Bharat Mediratta <bharat@menalto.com>
2002-02-16 21:37:55 +0000

    Bumped version to 1.2.6-b6, config_version to 28
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1523 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -38,8 +38,8 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2.6-b6";
-$gallery->config_version = 27;
+$gallery->version = "1.2.6-b7";
+$gallery->config_version = 28;
 $gallery->album_version = 4;
 $gallery->remote_protocol_version = 1;
 $gallery->url = "http://gallery.sourceforge.net";
