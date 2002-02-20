4f22f1290aff64bfbaeba21277e94e09ed30ed9f
Bharat Mediratta <bharat@menalto.com>
2002-02-20 06:40:51 +0000

    2002-02-19  Bharat Mediratta  <bharat@menalto.com> 1.2.6-cvs-b7
           * Minor tweak -- we now set an extended time limit when moving photos
             also.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1526 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -38,7 +38,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2.6-b7";
+$gallery->version = "1.2.6-b8";
 $gallery->config_version = 28;
 $gallery->album_version = 4;
 $gallery->remote_protocol_version = 1;
