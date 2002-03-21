0b209b91536a110af78a147f2b70401702848307
Bharat Mediratta <bharat@menalto.com>
2002-03-21 07:31:17 +0000

    2002-03-20  Bharat Mediratta  <bharat@menalto.com> 1.2.6-cvs-b32
            * Fixed a minor but annoying issue where the config wizard spouts out
              tons of "stat file does not exist" warnings when warnings are not
              dialed down on the server.
    
            * Added support for PostNuke 0.7.1's new user API.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1630 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -38,7 +38,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.2.6-cvs-b31";
+$gallery->version = "1.2.6-cvs-b32";
 $gallery->config_version = 30;
 $gallery->album_version = 4;
 $gallery->remote_protocol_version = 1;
