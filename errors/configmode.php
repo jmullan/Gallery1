68228aaac50ca5b6f8f66c11993e88abaca22811
Bharat Mediratta <bharat@menalto.com>
2002-07-30 09:05:47 +0000

    Added hack check code to prevent users from passing in the GALLERY_BASEDIR variable in the path and remotely executing commands.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1862 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- errors/configmode.php
+++ errors/configmode.php
@@ -1,3 +1,12 @@
+<?
+// Hack prevention.
+if (!empty($HTTP_GET_VARS["GALLERY_BASEDIR"]) ||
+                    !empty($HTTP_POST_VARS["GALLERY_BASEDIR"]) ||
+                    !empty($HTTP_COOKIE_VARS["GALLERY_BASEDIR"])) {
+            print "Security violation\n";
+	            exit;
+		    }
+?>
 <? require($GALLERY_BASEDIR . "errors/configure_instructions.php") ?>
 <html>
 <head>
