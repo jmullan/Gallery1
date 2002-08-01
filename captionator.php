a24042bc3aa939dc387b1251ba7b189615228a66
Bharat Mediratta <bharat@menalto.com>
2002-08-01 09:58:49 +0000

    Added GALLERY_BASEDIR hack prevention
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@1880 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- captionator.php
+++ captionator.php
@@ -18,6 +18,15 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 ?>
+<?
+// Hack prevention.
+if (!empty($HTTP_GET_VARS["GALLERY_BASEDIR"]) ||
+                    !empty($HTTP_POST_VARS["GALLERY_BASEDIR"]) ||
+                    !empty($HTTP_COOKIE_VARS["GALLERY_BASEDIR"])) {
+    print "Security violation\n";
+    exit;
+}
+?>
 <? require($GALLERY_BASEDIR . "init.php"); ?>
 <? 
 // Hack check
