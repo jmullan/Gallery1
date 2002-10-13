c06c457115064636353b51cef18c24ddd6ded977
Pierre-Luc Paour <pierre-luc@paour.com>
2002-10-13 11:20:05 +0000

    Added gallery_remote2.php, which is the initial version of the new Gallery Remote protocol v2. Right now it supports:  - uploading a caption with each picture  - fetching album properties  - better listing of albums
    It is also redesigned for better robustness and more informative error
    messages.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@2185 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ classes/remote/GalleryRemoteProperties.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * Gallery - a web based photo album viewer and editor
+ * Copyright (C) 2000-2002 Bharat Mediratta
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+?>
+<?php
+
+/*
+ * workaround for php < 4.1.0
+ */
+if ( phpversion() < '4.1' ) {
+eval('
+	function array_key_exists($key, $arr)
+	{
+		if (!is_array($arr)) {
+			return false;
+		}
+		foreach (array_keys($arr) as $k) {
+			if ($k == $key) {
+				return true;
+			}
+		}
+		return false;
+	}
+');
+}
+
+/**
+ *  This class partially mirrors the functionality of the java class
+ *	java.util.Properties.
+ *
+ *@author     tmiller
+ *@created    September 29, 2002
+ */
+class Properties {
+	var $map;
+	
+	function Properties( $defaults = null ) {
+		$this->map = $defaults;
+	}
+
+	function getProperty( $key, $defaultValue = null ) {
+		if ( array_key_exists( $key, $this->map ) ) {
+			return $this->map[ $key ];
+		} else {
+			return $defaultValue;
+		}
+	}
+	
+	function listprops() {	// list is a php reserved word
+		$ret = "#__GR2PROTO__\n";
+		foreach (array_keys($this->map) as $k) {
+			$ret .= "$k=" . $this->map[$k] . "\n";
+		}
+		return $ret;
+	}
+	
+	function setProperty( $key, $value ) {
+		if ( $key != null ) {
+			$this->map[$key] = $value;	
+		}
+	}
+}
+?>
