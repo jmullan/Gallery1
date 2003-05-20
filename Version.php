049869f5b021dc7c8fb1c67bb8c84215b4554621
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-05-20 16:20:29 +0000

    BUGFIX.  Upgrading of albums did not add extra field defaults
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@2742 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -38,9 +38,9 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.3.4-cvs-b30";
+$gallery->version = "1.3.4-cvs-b31";
 $gallery->config_version = 34;
-$gallery->album_version = 7;
+$gallery->album_version = 8;
 $gallery->remote_protocol_version = 1;
 $gallery->url = "http://gallery.sourceforge.net";
 
