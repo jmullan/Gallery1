7cfca4e1942192dec43c36b1a5d838ab854f36b4
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-07-14 21:47:12 +0000

    compatibility with 1.3.5
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/DEV_1_3_5_ML@3177 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/errors/configure_instructions.php
+++ gallery/errors/configure_instructions.php
@@ -7,17 +7,18 @@ function configure($cmd="configure") {
   <tr>
    <td>
     <code>
-     <br> <b>Unix</b> <?php echo _("with shell access") ; ?>
-     <br> % cd /<?php echo _("path") .'/'. _("to") .'/'. _("your") .'/'. _("gallery") ; ?>
+     <br> <b>Unix</b> <?php echo _("with shell access") ?>
+     <?php $pathstring = _("/path/to/your/gallery") ?>
+     <br> % cd <?php echo $pathstring ?>
      <br> % sh ./<?php echo $cmd ?>.sh
      <br>
-     <br> <b>Unix</b> <?php echo _("with FTP access") ; ?>
+     <br> <b>Unix</b> <?php echo _("with FTP access") ?>
      <br> ftp> chmod <?php echo configure_filemode($cmd) ?> .htaccess
      <br> ftp> chmod <?php echo configure_filemode($cmd) ?> config.php
      <br> ftp> chmod <?php echo configure_dirmode($cmd) ?> setup
      <br>
      <br> <b>Windows</b>
-     <br> C:\> cd \<?php echo _("path") .'\\'. _("to") .'\\'. _("your") .'\\'. _("gallery") ; ?>
+     <br> C:\> cd <?php echo strtr($pathstring, '/', '\\') ?>
      <br> C:\> <?php echo $cmd ?>.bat
      <br>
      <br>
