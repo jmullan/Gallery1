56e886affc7065c33d9d7bb3be22a19124e15497
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-07-21 15:55:34 +0000

    added $id to gallery.pot
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/DEV_1_3_5_ML@3257 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/po/create_po_template.sh
+++ gallery/po/create_po_template.sh
@@ -3,6 +3,6 @@
 
 #note: requires xgettext version 0.12.1 or greater
 
-xgettext --files-from=filelist -o gallery.pot -LPHP --keyword=_ --no-wrap
+echo '# $Id$' > gallery.pot
 
->> gallery.pot
+xgettext --files-from=filelist -o - -LPHP --keyword=_ --no-wrap >> gallery.pot
