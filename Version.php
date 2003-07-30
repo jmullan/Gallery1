8510bb696f90cd6dbb69082973bb3acffd734a22
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-07-30 14:26:30 +0000

    don't call array_map if it doesn't exist
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@3410 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.3.5-cvs-b66";
+$gallery->version = "1.3.5-cvs-b67";
 $gallery->config_version = 40;
 $gallery->album_version = 13;
 $gallery->remote_protocol_version = 1;
