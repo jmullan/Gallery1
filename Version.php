78863b25075341cee428b7e509590d18c2145a77
Andy Lindeman <alindeman@gmail.com>
2003-08-04 13:40:47 +0000

    revert
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@3498 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!$gallery) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.3.5-cvs-b91";
+$gallery->version = "1.3.5-cvs-b92";
 $gallery->config_version = 40;
 $gallery->album_version = 13;
 $gallery->remote_protocol_version = 1;
