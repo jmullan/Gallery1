63ff9ebdfc66916b1d8fceb14c8b01a0cfe3ee55
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-08-13 12:23:20 +0000

    1.3.5-cvs-b122 Change informational strings in Config. Wizard to conform to packaging decisions.  IE no language packs and README now basically content free.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@3593 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.3.5-cvs-b121";
+$gallery->version = "1.3.5-cvs-b122";
 $gallery->config_version = 42;
 $gallery->album_version = 14;
 $gallery->remote_protocol_version = 1;
