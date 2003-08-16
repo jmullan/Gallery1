acc7ab02f02db3151b7f7196b8f80e2d282ccf9c
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-08-16 22:10:00 +0000

    1.4-cvs-b132 Fixed bug so hopefully offline browsing is restricted to the album with which you started.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_4@3637 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.4-cvs-b131";
+$gallery->version = "1.4-cvs-b132";
 $gallery->config_version = 44;
 $gallery->album_version = 15;
 $gallery->remote_protocol_version = 1;
