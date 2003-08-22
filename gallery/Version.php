b23cc4ff39548a69b41763747310b8c437fc37cd
Jens Alexander Tkotz <jens@peino.de>
2003-08-22 22:19:39 +0000

    2003-08-23 Jens Tkotz  <jens@peino.de> 1.4-RC2-b3
    * Language Update: Slovanian (sl_SI)
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_4@3741 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.4-RC2-b2";
+$gallery->version = "1.4-RC2-b3";
 $gallery->config_version = 44;
 $gallery->album_version = 15;
 $gallery->remote_protocol_version = 1;
