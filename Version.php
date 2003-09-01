c32ace048c7456403eed84dc0551734a4b947c50
Andy Lindeman <alindeman@gmail.com>
2003-09-01 20:02:52 +0000

    2003-09-01  Andrew Lindeman  <joyoflinux@yahoo.com> 1.4.1-cvs-b47
            * Remove UPGRADING file, in favor of using the README to point
              users to the new documentation.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@3848 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.4.1-cvs-b46";
+$gallery->version = "1.4.1-cvs-b47";
 $gallery->config_version = 49;
 $gallery->album_version = 15;
 $gallery->remote_protocol_version = 1;
