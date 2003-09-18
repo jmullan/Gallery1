17e230d798cc17c03169a0aa225d526ab74805ff
Jens Alexander Tkotz <jens@peino.de>
2003-09-18 14:04:53 +0000

    2003-09-18  Jens Tkotz <jens@peino.de> 1.4.1-cvs-b78
            * Language updates: German (de_DE), Spanish (es_ES), Turkish (tr_TR)
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4001 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.4.1-cvs-b77";
+$gallery->version = "1.4.1-cvs-b78";
 $gallery->config_version = 58;
 $gallery->album_version = 19;
 $gallery->user_version = 1;
