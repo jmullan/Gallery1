766df1f11bb3ee71fa2dad14a3378171f29523e4
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-09-23 14:17:58 +0000

    2003-09-23 Joan McGalliard  <gallery@joanhenge.plus.com> 1.4.1-cvs-b88
    	* Adding of new_password.php.  Part of the lost password
    	  modification, and should have been added in b87,
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4060 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.4.1-cvs-b87";
+$gallery->version = "1.4.1-cvs-b88";
 $gallery->config_version = 61;
 $gallery->album_version = 19;
 $gallery->user_version = 2;
