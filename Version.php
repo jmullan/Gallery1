94ba03345a6fa9b341f1165964d4c44e7ca2cecd
Jens Alexander Tkotz <jens@peino.de>
2003-09-25 14:47:04 +0000

    2003-09-26  Jens Tkotz <jens@peino.de> 1.4.1-cvs-b98
            * Change: Use $borderColor instead of white when album_thumb_frame_style="dots"
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4084 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.4.1-cvs-b97";
+$gallery->version = "1.4.1-cvs-b98";
 $gallery->config_version = 64;
 $gallery->album_version = 19;
 $gallery->user_version = 3;
