55708a051d5c5b17aab1f1935c5ca28cfc9407fd
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-09-28 12:39:28 +0000

    2003-09-28 Joan McGalliard  <gallery@joanhenge.plus.com> 1.4.1-cvs-b109
    	* Removed all the pretty printing from html_wrap/inline_imagewrap.inc
    	  so it displays correctly in IE 6.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4111 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "1.4.1-cvs-b108";
+$gallery->version = "1.4.1-cvs-b109";
 $gallery->config_version = 66;
 $gallery->album_version = 21;
 $gallery->user_version = 3;
