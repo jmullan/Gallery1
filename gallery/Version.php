47f164c7b4b2cd6eccbbc52ee8b125b79e527986
Jens Alexander Tkotz <jens@peino.de>
2003-09-30 23:17:37 +0000

    changed Version Number to '48.1'
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_4@4304 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -41,7 +41,7 @@ if (!isset($gallery)) {
 }
 
 $gallery->version = "1.4-pl1-lang1";
-$gallery->config_version = 49;
+$gallery->config_version ='48.1';
 $gallery->album_version = '15.1';
 $gallery->remote_protocol_version = 1;
 $gallery->user_version = 1;
