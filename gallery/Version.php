91dec75845f3358ba6bc0f848e5a65f7f7f5c6b0
John Kirkland <jpk@bl.org>
2003-10-03 17:50:00 +0000

    2003-10-01  John Kirkland <jpk@bl.org> DEV_1_4_1_skins-cvs-b14
            * added attention, borderleft, borderright, & bordertop
              to all skins
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/DEV_1_4_1_skins@4355 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = "DEV_1_4_1_skins-cvs-b13";
+$gallery->version = "DEV_1_4_1_skins-cvs-b14";
 $gallery->config_version = "68.1";
 $gallery->album_version = 22;
 $gallery->user_version = 3;
