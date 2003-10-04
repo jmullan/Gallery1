7c9270f1944e3dd2f9ad9f3158b43feb4d98334f
Joan McGalliard <jefmcg@users.sourceforge.net>
2003-10-04 11:48:22 +0000

    2003-10-04 Joan McGalliard  <gallery@joanhenge.plus.com> 1.4.1-cvs-b124
    	* No longer require confirmation when changing highlight.
    
    	* Fixed bug where image dimensions didn't display under image.
    
    	* Added align="center" that beckett suggests to inline_imagewrap.inc.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4370 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- view_photo.php
+++ view_photo.php
@@ -701,7 +701,7 @@ if (is_int($key))
 
 	$dimensions=$photo->getDimensions($full);
 	$table .= "<tr><td valign=top align=right><b>".$automaticFields[$field].":</b></td><td>".
-	$dimensions[0]." x ".$dimensions[1]." (". (int) $photo->getFileSize($full) >> 10 ."k)</td></tr>";
+	$dimensions[0]." x ".$dimensions[1]." (". ((int) $photo->getFileSize($full) >> 10 )."k)</td></tr>";
 	unSet($extra_fields[$key]);
 }
 
