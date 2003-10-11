c37bc0c58452f1b8437dff0c449e66a123937012
Andy Lindeman <alindeman@gmail.com>
2003-10-11 20:25:04 +0000

    2003-10-11  Andrew Lindeman  <joyoflinux@yahoo.com> 1.4.1-cvs-b145
    	* Fixed a security issue in the setup/index.php file, which could be
    	  exploited maliciously to allow remote files to be included, leading
    	  to code injection.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4432 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.1-cvs-b144';
+$gallery->version = '1.4.1-cvs-b145';
 $gallery->config_version = 71;
 $gallery->album_version = 22;
 $gallery->user_version = 3;
