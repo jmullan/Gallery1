4403d9a02ba5b203a30ef4077942b5da07445c86
Beckett Madden-Woods <beckett@beckettmw.com>
2003-10-13 18:32:58 +0000

    2003-10-13  Beckett Madden-Woods  <beckett@beckettmw.com> 1.4.1-cvs-b155
            * Fixed my bad cut-n-paste carelessness in classes/Album.php.
    
            * Removed highlight_photo.php, which is no longer used.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4457 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.1-cvs-b154';
+$gallery->version = '1.4.1-cvs-b155';
 $gallery->config_version = 71;
 $gallery->album_version = 22;
 $gallery->user_version = 4;
