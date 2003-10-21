2985896c6663551db16a6699a3bde331e7227f5d
Jens Alexander Tkotz <jens@peino.de>
2003-10-21 21:45:05 +0000

    2003-10-21 Jens Tkotz <jens@peino.de> 1.4.1-cvs-b183
            * Bugfix: Added missing global $GALLERY_BASEDIR,
                      which causes flags not to showed.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4539 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- tools/g1-makereport.php
+++ tools/g1-makereport.php
@@ -7,6 +7,7 @@
 
 	$path=dirname(__FILE__);
 	shell_exec ("php $path/g1-report.php > $path/reports/$gallery->version--report.html");
+	shell_exec ("rm $path/g1-report.html");
 	shell_exec ("ln -s $path/reports/$gallery->version--report.html $path/g1-report.html");
 	
 ?>
