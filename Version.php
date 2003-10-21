187feb463850513a68b0a18791e058fd5b06a55d
Jens Alexander Tkotz <jens@peino.de>
2003-10-21 13:35:55 +0000

    2003-10-21 Jens Tkotz <jens@peino.de> 1.4.1-cvs-b182
            * Language: Fixed Czech Encodings (both)
                        This fixes Bug #821509
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4537 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.1-cvs-b181';
+$gallery->version = '1.4.1-cvs-b182';
 $gallery->config_version = 75;
 $gallery->album_version = 24;
 $gallery->user_version = 5;
