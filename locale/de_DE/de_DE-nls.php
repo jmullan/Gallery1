66949fdbbee38eea180974d648c06e06a080dfeb
Jens Alexander Tkotz <jens@peino.de>
2003-11-08 18:28:56 +0000

    test
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4632 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- locale/de_DE/de_DE-nls.php
+++ locale/de_DE/de_DE-nls.php
@@ -18,7 +18,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  * $Id$
- * $Name$ $Date$
+ * $Name$
+ * $Date$
  */
 /**
  * NLS (National Language System) array.
