a1873da7449ff46d025d4778e07d974a9d072abb
Beckett Madden-Woods <beckett@beckettmw.com>
2003-11-12 09:08:32 +0000

    2003-11-12  Beckett Madden-Woods  <beckett@beckettmw.com> 1.4.1-RC3
            * v1.4.1 Release Candidate 3
    
            * Added warning notice to upgraders in Config. Wizard Step 3.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_4_1@4673 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.1-RC3-b7';
+$gallery->version = '1.4.1-RC3';
 $gallery->config_version = 76;
 $gallery->album_version = 25;
 $gallery->user_version = 5;
