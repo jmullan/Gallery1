77b2949f9bf48a38f0d880db6815a1ce6eb8bfdf
Bharat Mediratta <bharat@menalto.com>
2003-11-28 11:49:03 +0000

    2003-11-28  Bharat Mediratta  <bharat@menalto.com> 1.4.2-cvs-b40
            * Update php_value template to use php_value on Apache2 and
              php_flag on Apache1 to get around a bug in Apache2.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@4838 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.2-cvs-b39';
+$gallery->version = '1.4.2-cvs-b40';
 $gallery->config_version = 76;
 $gallery->album_version = 25;
 $gallery->user_version = 5;
