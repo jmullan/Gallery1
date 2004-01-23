896a70e17cbc73d3995a7a58048cd38788616aad
Joan McGalliard <jefmcg@users.sourceforge.net>
2004-01-23 00:54:35 +0000

    2004-01-22  Joan McGalliard  <gallery@joanhenge.plus.com> 1.4.3-cvs-b1
            * This is the beginning of development on v1.4.3.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@5119 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.2-cvs-b78';
+$gallery->version = '1.4.3-cvs-b1';
 $gallery->config_version = 79;
 $gallery->album_version = 25;
 $gallery->user_version = 5;
