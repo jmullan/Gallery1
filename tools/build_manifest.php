ae4f04959ec462e742c96673f9f08234fe1dcea1
Bharat Mediratta <bharat@menalto.com>
2004-01-27 06:00:59 +0000

    2004-01-26  Bharat Mediratta  <bharat@menalto.com> 1.4.3-cvs-b8
            * Overhauled the build_manifest script so that it reads the CVS Entries
              file to find appropriate files to check for version ids.  This way we
              don't have to maintain a file list (which is error prone).
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@5161 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- tools/build_manifest.php
+++ tools/build_manifest.php
@@ -42,165 +42,8 @@ if (!fs_is_readable("setup")) {
 	print "\n";
 	exit (2);
 }
-$files=array(	"AUTHORS",
-	       	"LICENSE.txt",
-	       	"README",
-	       	"Version.php",
-	       	"add_comment.php",
-	       	"add_photos.php",
-	       	"adv_search.php",
-	       	"album_permissions.php",
-	       	"albums.php",
-	       	"captionator.php",
-	       	"configure.bat",
-	       	"configure.sh",
-	       	"copy_photo.php",
-	       	"create_user.php",
-	       	"delete_album.php",
-	       	"delete_photo.php",
-	       	"delete_user.php",
-	       	"do_command.php",
-	       	"edit_appearance.php",
-	       	"edit_caption.php",
-	       	"edit_field.php",
-	       	"edit_thumb.php",
-	       	"extra_fields.php",
-	       	"gallery_remote.php",
-	       	"gallery_remote2.php",
-	       	"index.php",
-	       	"init.php",
-	       	"login.php",
-	       	"manage_users.php",
-	       	"modify_user.php",
-	       	"move_album.php",
-	       	"move_photo.php",
-	       	"multi_create_user.php",
-	       	"new_password.php",
-	       	"nls.php",
-	       	"photo_owner.php",
-	       	"poll_properties.php",
-	       	"poll_results.php",
-	       	"progress_uploading.php",
-	       	"publish_xp.php",
-	       	"publish_xp_docs.php",
-	       	"register.php",
-	       	"rename_album.php",
-	       	"reset_votes.php",
-	       	"resize_photo.php",
-	       	"rotate_photo.php",
-	       	"save_photos.php",
-	       	"search.php",
-	       	"secure.bat",
-	       	"secure.sh",
-	       	"session.php",
-	       	"slideshow.php",
-	       	"slideshow_high.php",
-	       	"slideshow_low.php",
-	       	"sort_album.php",
-	       	"upgrade_album.php",
-	       	"upgrade_users.php",
-	       	"user_preferences.php",
-	       	"util.php",
-	       	"view_album.php",
-	       	"view_comments.php",
-	       	"view_photo.php",
-	       	"view_photo_properties.php",
-	       	"classes/Album.php",
-	       	"classes/AlbumDB.php",
-	       	"classes/AlbumItem.php",
-	       	"classes/Comment.php",
-	       	"classes/Database.php",
-	       	"classes/EverybodyUser.php",
-	       	"classes/Image.php",
-	       	"classes/LoggedInUser.php",
-	       	"classes/NobodyUser.php",
-	       	"classes/User.php",
-	       	"classes/UserDB.php",
-	       	"classes/database/mysql/Database.php",
-	       	"classes/gallery/User.php",
-	       	"classes/gallery/UserDB.php",
-	       	"classes/nuke5/AdminUser.php",
-	       	"classes/nuke5/User.php",
-	       	"classes/nuke5/UserDB.php",
-	       	"classes/postnuke/User.php",
-	       	"classes/postnuke/UserDB.php",
-	       	"classes/postnuke0.7.1/User.php",
-	       	"classes/postnuke0.7.1/UserDB.php",
-	       	"classes/remote/GalleryRemoteProperties.php",
-	       	"css/config.css.default",
-	       	"css/embedded_style.css.default",
-	       	"css/standalone_style.css.default",
-	       	"errors/configmode.php",
-	       	"errors/configure_help.php",
-	       	"errors/configure_instructions.php",
-	       	"errors/reconfigure.php",
-	       	"errors/unconfigured.php",
-	       	"html/errorRow.inc",
-	       	"html/userData.inc",
-	       	"html_wrap/album.footer.default",
-	       	"html_wrap/album.header.default",
-	       	"html_wrap/gallery.footer.default",
-	       	"html_wrap/gallery.header.default",
-	       	"html_wrap/inline_albumthumb.footer.default",
-	       	"html_wrap/inline_albumthumb.frame.default",
-	       	"html_wrap/inline_albumthumb.header.default",
-	       	"html_wrap/inline_gallerythumb.frame.default",
-	       	"html_wrap/inline_imagewrap.inc",
-	       	"html_wrap/inline_moviethumb.frame.default",
-	       	"html_wrap/inline_photo.footer.default",
-	       	"html_wrap/inline_photo.frame.default",
-	       	"html_wrap/inline_photo.header.default",
-	       	"html_wrap/inline_photothumb.frame.default",
-	       	"html_wrap/photo.footer.default",
-	       	"html_wrap/photo.header.default",
-	       	"html_wrap/search.footer.default",
-	       	"html_wrap/search.header.default",
-	       	"html_wrap/slideshow.footer.default",
-	       	"html_wrap/slideshow.header.default",
-	       	"html_wrap/wrapper.footer.default",
-	       	"html_wrap/wrapper.header.default",
-	       	"html_wrap/frames/README.php",
-	       	"html_wrap/frames/polaroid/frame.def",
-	       	"html_wrap/frames/polaroids/frame.def",
-	       	"html_wrap/frames/shadows/frame.def",
-	       	"html_wrap/frames/simple_book/frame.def",
-	       	"js/client_sniff.js",
-	       	"layout/adminbox.inc",
-	       	"layout/breadcrumb.inc",
-	       	"layout/commentbox.inc",
-	       	"layout/commentboxbottom.inc",
-	       	"layout/commentboxtop.inc",
-	       	"layout/commentdraw.inc",
-	       	"layout/ml_pulldown.inc",
-	       	"layout/navigator.inc",
-	       	"layout/navphoto.inc",
-		"layout/navtablebegin.inc",
-		"layout/navtableend.inc",
-		"layout/navtablemiddle.inc",
-	       	"layout/searchdraw.inc",
-	       	"platform/fs_unix.php",
-	       	"platform/fs_win32.php",
-	       	"setup/backup_albums.php",
-	       	"setup/check.inc",
-	       	"setup/check_imagemagick.php",
-	       	"setup/check_mail.php",
-	       	"setup/check_netpbm.php",
-	       	"setup/check_versions.php",
-	       	"setup/config_data.inc",
-	       	"setup/confirm.inc",
-	       	"setup/constants.inc",
-	       	"setup/defaults.inc",
-	       	"setup/diagnostics.php",
-	       	"setup/functions.inc",
-	       	"setup/gpl.txt",
-	       	"setup/index.php",
-	       	"setup/init.php",
-	       	"setup/mod_rewrite.template",
-	       	"setup/php_value.template",
-	       	"setup/php_value_ok.php",
-	       	"setup/phpinfo.php",
-	       	"setup/session_test.php",
-	       	"setup/write.inc");
+
+$files = getManifestFiles(".");
 
 $outfile="manifest.inc";
 copy("setup/gpl.txt", $outfile);
@@ -210,30 +53,50 @@ fwrite($fd, "<?php\n\n");
 fwrite($fd, "/*\n * DO NOT EDIT!!!  This file is created by build_manifest.php.\n * Edit that file and re-run via command line to modify this.\n */\n\n");
 fwrite($fd, "\$versions=array();\n");
 
-$error=false;
-foreach ($files as $file) {
-       	$version=getCVSVersion($file);
-       	if ($version === NULL) {
-	       	print "ERROR: $file missing\n";
-		$error=true;
-       	} else if ($version === "") {
-	       	print "ERROR: \$id missing from $file\n";
-		$error=true;
-       	} else {
-	       	fwrite($fd, "\$versions['$file']='$version';\n");
-       	}
+ksort($files);
+foreach ($files as $file => $version) {
+    /*
+     * There are some exceptions.  We don't want to require these directories
+     *   po
+     *   tools
+     *
+     * because they're not critical to the proper functioning of gallery
+     */
+    if (preg_match('{^(po|tools)/}', $file)) {
+	continue;
+    }
+    
+    fwrite($fd, "\$versions['$file']='$version';\n");
 }	
 
 fwrite($fd, "?>\n");
 fclose($fd);
-if (!$error) {
-	print "Done\n";
-	exit(0);
-} else {
-	print "Please fix errors and re-run\n";
-	exit(1);
+print "Done\n";
+
+function getManifestFiles($dir) {
+    $results = array();
+    
+    if (fs_file_exists("$dir/CVS/Entries")) {
+	$fd = fs_fopen("$dir/CVS/Entries", "rb");
+	while ($line = fgets($fd, 4096)) {
+	    if ($line[0] == 'D') {
+		preg_match('|^D/(.*?)/|', $line, $regs);
+		if (!empty($regs)) {
+		    $results = array_merge($results, getManifestFiles("$dir/$regs[1]"));
+		}
+	    } else {
+		preg_match('|^/(.*?)/|', $line, $regs);
+		$file = substr("$dir/$regs[1]", 2);
+		$version = getCVSVersion($file);
+		if (!empty($version)) {
+		    $results[$file] = $version;
+		}
+	    }
+	}
+	fclose($fd);
+    }
+
+    return $results;
 }
-
-
 
 ?>
