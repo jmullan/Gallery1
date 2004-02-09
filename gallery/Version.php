552b88b6cb20e8454dacbfaf099bb6c9a27f7e75
Jens Alexander Tkotz <jens@peino.de>
2004-02-09 14:59:49 +0000

    2004-02-09 Jens Tkotz <jens@peino.de> 1.4.2-RC3-cvs-b1
            * Remove some "new in 1.4.1".
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_4_2@5229 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -40,7 +40,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.2-RC2';
+$gallery->version = '1.4.2-RC3-cvs-b1';
 $gallery->config_version = 79;
 $gallery->album_version = 25;
 $gallery->user_version = 5;
