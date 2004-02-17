6512f55bb395832150912b8e9fe97dda100277e0
Jens Alexander Tkotz <jens@peino.de>
2004-02-17 14:56:44 +0000

    2004-02-17 Jens Tkotz <jens@peino.de> 1.4.3-cvs-b37
            * Change: Adjust secure.sh and config.sh to fit with Debian.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@5276 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- configure.sh
+++ configure.sh
@@ -1,15 +1,48 @@
 #!/bin/sh
 # $Id$
 
-chmod 755 setup
+set -e
 
-if [ ! -f config.php ]; then
-    touch config.php
-fi
-if [ ! -f .htaccess ]; then
-    touch .htaccess
-fi
-chmod 666 config.php .htaccess
+# Note: These Gallery path are valid for 1.4.2
+
+GALLERY_DEB_ROOT=/usr/share/gallery
+GALLERY_DEB_CONFDIR=/etc/gallery
+GALLERY_DEB_SETUPDIR=/var/lib/gallery/setup
+
+#
+# Set Pathes depending on Environment
+#
+if [ -d $GALLERY_DEB_ROOT ] && [ -d $GALLERY_DEB_CONFDIR ] ; then
+	# It seems were in Debian, where Gallery has a different structure.
+	if [ `whoami` != "root" ] ; then
+		echo "You must be root to run this script" 2>&1
+		exit 1
+	fi
+
+	GALLERY_ROOT=$GALLERY_DEB_ROOT
+	GALLERY_CONFDIR=$GALLERY_DEB_CONFDIR
+else
+	# We are in a "normal installation"
+	GALLERY_ROOT="."
+	GALLERY_SETUPDIR="./setup"
+fi	
+	
+
+# Set Permissions
+for file in $GALLERY_CONFDIR/config.php $GALLERY_CONFDIR/htaccess ; do
+	if [ ! -f $file ]; then
+        	touch $file
+	fi
+    
+	if [ -d $GALLERY_DEB_ROOT ] && [ -d $GALLERY_DEB_CONFDIR ] ; then
+		chown www-data:root $file
+	fi
+	
+	chmod 755 $file
+done
+
+chmod 755 $GALLERY_ROOT/setup
+
 
 echo ""
 echo "You are now in setup mode, which is *INSECURE*.  Your Gallery"
@@ -19,5 +52,5 @@ echo ""
 echo "When you are done with your installation, don't forget to"
 echo "run the secure.sh script!"
 echo ""
-echo "    % sh secure.sh"
+echo "    # sh $GALLERY_ROOT/secure.sh"
 echo ""
--- secure.sh
+++ secure.sh
@@ -1,19 +1,52 @@
 #!/bin/sh
 # $Id$
 
-chmod 0 setup
+set -e
 
-if [ -f config.php ]; then
-    chmod 644 config.php 
-fi
+# Note: These Gallery path are valid for 1.4.2
 
-if [ -f .htaccess ]; then
-    chmod 644 .htaccess
-fi
+GALLERY_DEB_ROOT=/usr/share/gallery
+GALLERY_DEB_CONFDIR=/etc/gallery
+GALLERY_DEB_SETUPDIR=/var/lib/gallery/setup
+
+#
+# Set Pathes depending on Environment
+#
+if [ -d $GALLERY_DEB_ROOT ] && [ -d $GALLERY_DEB_CONFDIR ] ; then
+	# It seems were in Debian, where Gallery has a different structure.
+	if [ `whoami` != "root" ] ; then
+		echo "You must be root to run this script" 2>&1
+		exit 1
+	fi
+
+	GALLERY_ROOT=$GALLERY_DEB_ROOT
+	GALLERY_CONFDIR=$GALLERY_DEB_CONFDIR
+else
+	# We are in a "normal installation"
+	GALLERY_ROOT="."
+	GALLERY_SETUPDIR="./setup"
+fi	
+
+
+# Set Permissions
+for file in $GALLERY_CONFDIR/config.php $GALLERY_CONFDIR/htaccess ; do
+	if [ -s $file ] ; then
+		if [ -d $GALLERY_DEB_ROOT ] && [ -d $GALLERY_DEB_CONFDIR ] ; then
+        		chown root:root $file
+		else
+			chmod 644 $file
+		fi
+	else
+        	# the file is 0 bytes, remove it as it didn't get written out
+		rm -f $file
+	fi
+done
+
+chmod 0 $GALLERY_ROOT/setup
 
 echo ""
 echo "Your Gallery is now secure and cannot be configured.  If"
 echo "you wish to reconfigure it, run:"
 echo ""
-echo "    % sh configure.sh"
+echo "    % sh $GALLERY_ROOT/configure.sh"
 echo ""
