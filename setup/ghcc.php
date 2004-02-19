0e97fd014b1306c7e301c16692aefc1a61cfbc35
Andy Lindeman <alindeman@gmail.com>
2004-02-19 03:38:32 +0000

    2004-02-18  Andrew Lindeman <joyoflinux@yahoo.com> 1.4.3-cvs-b40         * Rewrote the setup/ghcc.php script.  It's still not linked from           anywhere just yet.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@5295 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- setup/ghcc.php
+++ setup/ghcc.php
@@ -23,20 +23,45 @@
  */
 ?>
 <?php
-exit();
-$tests = array ();
 
-// First element is the test, second is if it's fatal or not
+/*
+desc - A description of the test
+test - Boolean value of the result, true = good, false = bad
+failmsg - Message to display on fail
+sev - Severity of a fail, 0 = warning, 1 = fatal
+*/
 
-$tests['safe mode disabled'] = array ( (ini_get ('safe_mode') == ''), true);
-$tests['exec() function enabled'] = array (! (in_array ('exec', split (',\s*', ini_get ('disable_functions')))), true);
-$tests['file_uploads enabled'] = array ( (ini_get ('file_uploads') != ''), true);
-$tests['session.save_path writable'] = array (( is_dir (ini_get ('session.save_path')) && is_writable (ini_get ('session.save_path')) ), true);
-$tests['session.use_cookies on'] = array ( (ini_get ('session.use_cookies') != ''), true);
-$tests['allow_url_fopen on'] = array ( (ini_get ('allow_url_fopen') != ''), 'Gallery will not be able to fetch pictures from remote hosts');
+$tests = array (0 => array ("desc" => "safe mode",
+			    "test" => (ini_get ('safe_mode') == ''),
+			    "failmsg" => "<b>safe_mode</b> must be disabled in php.ini",
+			    "sev" => 1),
+		1 => array ("desc" => "exec() enabled",
+			    "test" => (! (in_array ('exec', split (',\s*', ini_get ('disable_functions'))))),
+			    "failmsg" => "The <b>exec()</b> function must not be disabled by the <b>disabled_functions</b> parameter in php.ini",
+			    "sev" => 1),
+		2 => array ("desc" => "file_uploads enabled",
+			    "test" => (ini_get ('file_uploads') != ''),
+			    "failmsg" => "<b>file_uploads</b> must be enabled in php.ini",
+			    "sev" => 1),
+		3 => array ("desc" => "session.save_path writable",
+			    "test" => ( is_dir (session_save_path()) && is_writable (session_save_path()) ),
+			    "failmsg" => "<b>session.save_path</b> must be set to a valid directory and be writable by the web server user",
+			    "sev" => 1),
+		4 => array ("desc" => "session.use_cookies enabled",
+			    "test" => (ini_get ('session.use_cookies') != ''),
+			    "failmsg" => "<b>session.use_cookies must be enabled in php.ini",
+			    "sev" => 1),
+		5 => array ("desc" => "allow_url_fopen on",
+			    "test" => (ini_get ('allow_url_fopen') != ''),
+			    "failmsg" => "Gallery will not be able to fetch pictures from remote hosts",
+			    "sev" => 0),
+		);
 
 if (ini_get ('upload_tmp_dir')) { // it defaults to a default system location, only test if this is set
-       	$tests['upload_tmp_dir writable'] = array (( is_dir (ini_get ('upload_tmp_dir')) && is_writable (ini_get ('upload_tmp_dir')) ), true);
+       	$tests[] = array ("desc" => "upload_tmp_dir writable",
+			  "test" => ( is_dir (ini_get ('upload_tmp_dir')) && is_writable (ini_get ('upload_tmp_dir')) ),
+			  "failmsg" => "<b>upload_tmp_dir</b> must be set to a valid directory and be writable by the web server user",
+			  "sev" => 1);
 }
 
 ?>
@@ -49,24 +74,41 @@ ImageMagick and NetPBM (or other programs Gallery can use)</i>
 If any of these tests fail with a <b>fatal warning</b>, Gallery
 will not run on your host
 <hr width="50%">
-<table>
+<table cellpadding="5">
 <?php
-foreach ($tests as $desc => $pass) {
+$warnings = false;
+$fatals = false;
+foreach ($tests as $arr) {
        	print '<tr><th>';
-       	print $desc;
+       	print $arr['desc'];
        	print '</th>';
 
 	print '<td>';
+	print str_repeat ('&nbsp;', 10);
+	print '<td>';
 
-	if ($pass[0] && $pass[1]) {
-	       	print '<font color="#00bb00">Pass</font>';
-       	} else if (! $pass[0] && $pass[1] !== true) {
-	       	print '<font color="#ff7050">Warning</font> -- '.$pass[1];
+	if ($arr['test']) {
+	       	print '<font color="#00aa00">Pass</font>';
+       	} else if (! $arr['sev']) {
+	       	print '<font color="#e0850f">Warning -- '.$arr['failmsg'] . '</font>';
+		$warnings = true;
        	} else {
-	       	print '<font color="#bb0000">Fail</font>';
+	       	print '<font color="#bb0000">Fatal Warning -- ' . $arr['failmsg'] . '</font>';
+		$fatals = true;
        	}
 
 	print '</td></tr>';
 }
 ?>
 </table>
+<br>
+<?php
+print '<b>Final Status Report: </b>';
+if ($fatals) {
+	print '<font color="#bb0000">Your PHP configuration flagged a <b>fatal warning</b>.  <b>Gallery will not run on this host without modifications to your PHP configuration</b>';
+} else if ($warnings) {
+	print '<font color="#e0850f">Your PHP configuration flagged some warnings (Gallery may lose some functionality)</font>';
+} else {
+	print '<font color="#00aa00">Your PHP configuration check passed with flying colors!</font>';
+}	
+?>
