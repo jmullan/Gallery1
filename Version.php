ea85ec6196baaf84a726d983397ec9ebf566ab7b
Jay Rossiter <signe@cothlamadh.net>
2004-04-22 20:42:22 +0000

    2004-04-22  Jay Rossiter <cryptographite@users.sf.net>  1.4.4-cvs-b50
            * Change: Make 'high' the default slideshow mode intead of 'applet'
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@5879 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.4-cvs-b49';
+$gallery->version = '1.4.4-cvs-b50';
 $gallery->config_version = 80;
 $gallery->album_version = 27;
 $gallery->user_version = 5;
