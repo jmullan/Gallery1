fa964fef042489eef3a4a0e7b6e6206e9e357fd3
Jay Rossiter <signe@cothlamadh.net>
2004-05-04 07:34:43 +0000

    2004-05-04  Jay Rossiter <cryptographite@users.sf.net>  1.4.4-cvs-b91
            * Change: Add an isset() to inline_imagewrap.inc
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@6017 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.4-cvs-b90';
+$gallery->version = '1.4.4-cvs-b91';
 $gallery->config_version = 80;
 $gallery->album_version = 27;
 $gallery->user_version = 5;
