ba10462a7010edef95f2065a520529a282e190c0
Jay Rossiter <signe@cothlamadh.net>
2004-05-30 22:39:42 +0000

    2004-05-30  Jay Rossiter <cryptographite@users.sf.net>  1.4.4-cvs-b160
            * Change: GUID needs to be added to new albums on creation, otherwise
              the RSS is generated with a blank guid field (if no photos have been
              added to the new album)
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@6206 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.4-cvs-b159';
+$gallery->version = '1.4.4-cvs-b160';
 $gallery->config_version = 80;
 $gallery->album_version = 29;
 $gallery->user_version = 5;
