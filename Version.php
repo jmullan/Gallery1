f2004c1db9184a5db56416ce3057b4981aec845b
Jens Alexander Tkotz <jens@peino.de>
2004-06-23 10:41:32 +0000

    2004-06-23 Jens Tkotz <jens@peino.de>  1.4.4-cvs-b212
            * Fix: Added missing ereg Paramater introduced in b210
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@6426 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.4-cvs-b211';
+$gallery->version = '1.4.4-cvs-b212';
 $gallery->config_version = 82;
 $gallery->album_version = 30;
 $gallery->user_version = 5;
