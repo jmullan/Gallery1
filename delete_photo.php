7592fc0d3ca041bd735579717fe51b552f89bf3a
Chris Kelly <ckdake@ckdake.com>
2004-06-23 16:42:05 +0000

    2004-06-23 Chris Kelly <ckdake@users.sf.net>  1.4.4-cvs-b212         * Fix: quoted attribute for better HTML compliance
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@6427 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- delete_photo.php
+++ delete_photo.php
@@ -110,7 +110,7 @@ $myAlbum->load($id);
 <br>
 <?php echo makeFormIntro("delete_photo.php"); ?>
 <input type="hidden" name="id" value="<?php echo $id ?>">
-<input type="hidden" name="albumDelete" value=<?php echo $myAlbum->fields['guid']; ?>>
+<input type="hidden" name="albumDelete" value="<?php echo $myAlbum->fields['guid']; ?>">
 <input type="submit" name="confirm" value="<?php echo _("Delete") ?>">
 <input type="button" name="cancel" value="<?php echo _("Cancel") ?>" onclick='parent.close()'>
 </form>
