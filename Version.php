75161cf64080923f4af2108579b7b620fed2916e
Jay Rossiter <signe@cothlamadh.net>
2004-07-01 18:30:09 +0000

    2004-07-01  Jay Rossiter <cryptographite@users.sf.net>  1.4.4-cvs-b233
            * 969218: Attempt to prevent detection of binaries that don't
              actually exist.  Make sure the located path is not empty.
    
            * Change: Whitespace, don't call phpversion() again, it's already
              in a local variable.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@6488 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.4-cvs-b232';
+$gallery->version = '1.4.4-cvs-b233';
 $gallery->config_version = 82;
 $gallery->album_version = 30;
 $gallery->user_version = 5;
