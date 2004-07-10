1c613014eda80285fb9b3291016e6183768e1eaf
Chris Kelly <ckdake@ckdake.com>
2004-07-10 18:02:46 +0000

    2004-07-10  Chris Kelly <ckdake@users.sf.net> 1.4.4-RC1
            * v1.4.4 Release Candidate 1
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_4_4@6587 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.4-cvs-b248';
+$gallery->version = '1.4.4-RC1';
 $gallery->config_version = 82;
 $gallery->album_version = 30;
 $gallery->user_version = 5;
