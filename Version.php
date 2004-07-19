30dd0f33cbdf90333f0c4c058e286ceb420c60c2
Jens Alexander Tkotz <jens@peino.de>
2004-07-19 00:00:14 +0000

    2004-07-19 Jens A. Tkotz <jens@peino.de> 1.4.5-cvs-b38
            * Change: Added link to JoEllen Drazans Skin Page in ConfigWizard.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@6798 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.5-cvs-b37';
+$gallery->version = '1.4.5-cvs-b38';
 $gallery->config_version = 82;
 $gallery->album_version = 31;
 $gallery->user_version = 5;
