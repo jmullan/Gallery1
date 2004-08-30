1933c30a59c5627177d7f3de27e7d7b11bfcdc5e
Jens Alexander Tkotz <jens@peino.de>
2004-08-30 15:08:44 +0000

    2004-08-30  Jens A. Tkotz <jens@peino.de>  1.4.5-cvs-b142
            * Change: Content of popup for uploading photos is not hardcode centered.
    
            * Change: No Alttext "No Caption", when Picture has no caption. RFE #962176.
    
            * Change: HTML Output beautify for add_photos form.
    
            * Change: Update Jenskin.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@7387 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.5-cvs-b141';
+$gallery->version = '1.4.5-cvs-b142';
 $gallery->config_version = 83;
 $gallery->album_version = 32;
 $gallery->user_version = 5;
