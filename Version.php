e966d55535b787ba9a6932bd99c4605750d82ad6
Jay Rossiter <signe@cothlamadh.net>
2004-09-07 23:53:41 +0000

    2004-09-07  Jay Rossiter <cryptographite@users.sf.net>  1.4.5-cvs-b160
            * Fix: Add register_globals check to config wizard
    
            * Fix: De-globalize login.inc
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@7492 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.4.5-cvs-b159';
+$gallery->version = '1.4.5-cvs-b160';
 $gallery->config_version = 83;
 $gallery->album_version = 32;
 $gallery->user_version = 5;
