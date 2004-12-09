d06651dbd4b10650ba0502168f242323386675f3
Christian Mohn <h0bbel@p0ggel.org>
2004-12-09 17:45:18 +0000

    2004-12-09  Christian Mohn <h0bbel@p0ggel.org>  1.5-cvs-b302
            * Fix: Typo in watermarking section of setup/config_data.inc
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@8453 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.5-cvs-b301';
+$gallery->version = '1.5-cvs-b302';
 $gallery->config_version = 86;
 $gallery->album_version = 33;
 $gallery->user_version = 5;
