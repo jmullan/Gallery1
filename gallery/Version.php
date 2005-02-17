07fdd731462dcc7f4b29d7409eb7a336801f1d69
Jay Rossiter <signe@cothlamadh.net>
2005-02-17 08:50:38 +0000

    2005-02-17  Jay Rossiter <cryptographite@users.sf.net>  1.5-RC1-cvs-b8
            * Change: Add a new config option for bypassing our register_globals-disabled
              emulation.  I do this unwillingly, and it is not a supported
              configuration due to the potential for security issues and other problems
              bug some people require the option.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_5@9043 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.5-RC1-cvs-b7';
+$gallery->version = '1.5-RC1-cvs-b8';
 $gallery->config_version = 90;
 $gallery->album_version = 33;
 $gallery->user_version = 5;
