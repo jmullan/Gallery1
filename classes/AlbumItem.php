c682978d8f434a7676af8863d571c6495b56ae3d
Jay Rossiter <signe@cothlamadh.net>
2005-03-10 02:01:20 +0000

    2005-03-09  Jay Rossiter <cryptographite@users.sf.net>  1.5-RC2-cvs-b31
            * Fix: Only try to watermark if the item isn't an album or a movie
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@9321 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- classes/AlbumItem.php
+++ classes/AlbumItem.php
@@ -492,7 +492,7 @@ class AlbumItem {
         function watermark($dir, $wmName, $wmAlphaName, $wmAlign, $wmAlignX, $wmAlignY, $preview=0, $previewSize=0, $wmSelect=0) {
                 global $gallery;
                 $type = $this->image->type;
-		if (isMovie($type))
+		if (isMovie($type) || $this->isAlbum())
 		{
 			// currently there is no watermarking support for movies
 			return (0);
