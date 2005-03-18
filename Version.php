9c5c5b20505abcced94e88a0ad3eda0795bce2ff
Jens Alexander Tkotz <jens@peino.de>
2005-03-18 09:34:42 +0000

    2005-03-18  Jens A. Tkotz <jens@peino.de>  1.5-cvs-RC3-b10
            * Fix: The headers in the b4 fix was not concatenated.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@9446 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.5-RC3-cvs-b9';
+$gallery->version = '1.5-RC3-cvs-b10';
 $gallery->config_version = 90;
 $gallery->album_version = 33;
 $gallery->user_version = 5;
