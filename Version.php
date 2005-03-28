2cd05877f119e4cc839926158a4c2b24edaafaa2
Jens Alexander Tkotz <jens@peino.de>
2005-03-28 21:41:20 +0000

    2005-03-28  Jens A. Tkotz <jens@peino.de>  1.5-cvs-RC3-b20
            * Change: Wording on view_album.
                    "admin options" -> "album actions"
                    "edit ..." -> "... actions"
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@9560 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
 	$gallery = new stdClass();
 }
 
-$gallery->version = '1.5-RC3-cvs-b19';
+$gallery->version = '1.5-RC3-cvs-b20';
 $gallery->config_version = 90;
 $gallery->album_version = 33;
 $gallery->user_version = 5;
