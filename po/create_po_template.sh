769d837949039e3ce4bcedb0b7ecedaeb631731d
Jens Alexander Tkotz <jens@peino.de>
2005-08-08 15:11:41 +0000

    Updated to new format.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@11249 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- po/create_po_template.sh
+++ po/create_po_template.sh
@@ -8,13 +8,11 @@
 
 
 ##### CORE .pot ############
-echo '# $Id$' > gallery-core.pot
-cat copyright.txt >> gallery-core.pot
+cat copyright-header.txt > gallery-core.pot
 
 xgettext --files-from=filelist-core -LPHP --keyword=_ --no-wrap --msgid-bugs-address="gallery-translations@lists.sourceforge.net" -o - | tail +7 >> gallery-core.pot
 
 ##### CONFIG .pot
-echo '# $Id$' > gallery-config.pot
-cat copyright.txt >> gallery-config.pot
+cat copyright-header.txt > gallery-config.pot
 
 xgettext --files-from=filelist-config -LPHP --keyword=_ --no-wrap --msgid-bugs-address="gallery-translations@lists.sourceforge.net" -o - | tail +7 >> gallery-config.pot
