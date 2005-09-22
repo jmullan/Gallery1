459e4f660c36a4dc4eaa4c92d16f7b73aaacbeb7
Jens Alexander Tkotz <jens@peino.de>
2005-09-22 21:29:54 +0000

    2005-09-22 Chris Kelly <ckdake@users.sf.net> 1.5.1
            * Release:  1.5.1
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@11666 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -31,7 +31,7 @@ if (!isset($gallery)) {
     $gallery = new stdClass();
 }
 
-$gallery->version = '1.5.1-RC4-cvs-b13';
+$gallery->version = '1.5.1';
 $gallery->config_version = 93;
 $gallery->album_version = 37;
 $gallery->user_version = 6;
