ae90f96bd15df4486e742ebe07c366246e48722c
Jens Alexander Tkotz <jens@peino.de>
2006-03-27 16:12:57 +0000

    2006-03-27 Jens Tkotz <jens@peino.de> 1.5.3-cvs-b17
     * Fix: In some dropdown boxes the wrong items is selected.
            Appears e.g. in slideshow in IE where "blend" should be selected, but acutally
            "RANDOM" is.
    
            Thanks to Yuan from forums.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_5_LEGACY@13344 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/lib/Form.php
+++ gallery/lib/Form.php
@@ -120,7 +120,7 @@ function drawSelect($name, $options, $selected, $size, $attrList = array(), $pre
                     $sel = ' selected';
                 }
             }
-            else if (!strcmp($value, $selected) || !strcmp($text, $selected) || $selected == '__ALL__') {
+            else if ($value == $selected || $text == $selected || $selected === '__ALL__') {
                 $sel = ' selected';
             }
             $buf .= "<option value=\"$value\"$sel>". $text ."</option>" . $crlf;
