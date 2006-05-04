ccd4d1abf46ad1ee15d92eedb6bb1b17365a39c3
Jens Alexander Tkotz <jens@peino.de>
2006-05-04 10:35:36 +0000

    Change $RCSfile$ to $Id$
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_5_LEGACY@13594 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/tools/g1-report.php
+++ gallery/tools/g1-report.php
@@ -1,7 +1,5 @@
 <?php
 /*
- * $RCSfile$
- *
  * Gallery - a web based photo album viewer and editor
  * Copyright (C) 2000-2006 Bharat Mediratta
  * 
@@ -18,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * $Id$
  */
 
 /**
