bdbc7c736c5d9f6ef5828c22d6b9515dff6453ff
Jens Alexander Tkotz <jens@peino.de>
2006-05-21 18:35:10 +0000

    Just indenting.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/DEV_1_6@13708 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/lib/mail.php
+++ gallery/lib/mail.php
@@ -2,23 +2,23 @@
 /*
  * Gallery - a web based photo album viewer and editor
  * Copyright (C) 2000-2006 Bharat Mediratta
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or (at
  * your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * $Id$
- */
+*/
 
 /**
  * @package	Mail
@@ -40,9 +40,10 @@ function check_email($email) {
 
 function emailDisclaimer() {
 	global $gallery;
+
 	$msg = unhtmlentities(sprintf(_("Note: This is an automatically generated email message sent from the %s website.  If you have received this in error, please ignore this message."),$gallery->app->photoAlbumURL).
-		"  \r\n".
-		sprintf(_("Report abuse to %s"),$gallery->app->adminEmail));
+	  "  \r\n".
+	  sprintf(_("Report abuse to %s"),$gallery->app->adminEmail));
 	$msg2 = sprintf("Note: This is an automatically generated email message sent from the %s website.  If you have received this in error, please ignore this message.  \r\nReport abuse to %s",
 	$gallery->app->photoAlbumURL, $gallery->app->adminEmail);
 
@@ -61,6 +62,7 @@ function emailDisclaimer() {
  */
 function gallery_mail($to, $subject, $msg, $logmsg, $hide_recipients = false, $from = NULL, $isNotifyMail = false, $isHTML = false) {
 	global $gallery;
+
 	$bcc = array();
 
 	if(!is_array($to)) {
@@ -122,12 +124,12 @@ function gallery_mail($to, $subject, $msg, $logmsg, $hide_recipients = false, $f
 	$gallery_mail->setHeadCharset($gallery->charset);
 
 	if($isHTML) {
-	    $gallery_mail->setHtmlCharset($gallery->charset);
-        $gallery_mail->setHtml($msg, _("This is a HTML mail, please have a look at the Attachment."));
+		$gallery_mail->setHtmlCharset($gallery->charset);
+		$gallery_mail->setHtml($msg, _("This is a HTML mail, please have a look at the Attachment."));
 	}
 	else {
-	    $gallery_mail->setText($msg);
-	    $gallery_mail->setTextCharset($gallery->charset);
+		$gallery_mail->setText($msg);
+		$gallery_mail->setTextCharset($gallery->charset);
 	}
 	$gallery_mail->setFrom($from);
 	$gallery_mail->setReturnPath($reply_to);
@@ -139,12 +141,12 @@ function gallery_mail($to, $subject, $msg, $logmsg, $hide_recipients = false, $f
 
 	if ($gallery->app->useOtherSMTP == "yes") {
 		$gallery_mail->setSMTPParams(
-			$gallery->app->smtpHost,
-			$gallery->app->smtpPort,
-			$gallery->app->smtpUserName,
-			FALSE,
-			$gallery->app->smtpUserName,
-			$gallery->app->smtpPassword
+		$gallery->app->smtpHost,
+		$gallery->app->smtpPort,
+		$gallery->app->smtpUserName,
+		FALSE,
+		$gallery->app->smtpUserName,
+		$gallery->app->smtpPassword
 		);
 	}
 
@@ -170,14 +172,14 @@ Congratulations.  You have just been subscribed to %s at %s.  Your account name 
 Gallery @ %s Administrator.");
 	if ($show_default) {
 		return sprintf($default,
-			"<b><nobr>&lt;" . _("gallery title") . "&gt;</nobr></b>",
-			"<b><nobr>&lt;" . _("gallery URL") . "&gt;</nobr></b>",
-			"<b><nobr>&lt;" . _("gallery title") . "&gt;</nobr></b>");
+		"<b><nobr>&lt;" . _("gallery title") . "&gt;</nobr></b>",
+		"<b><nobr>&lt;" . _("gallery URL") . "&gt;</nobr></b>",
+		"<b><nobr>&lt;" . _("gallery title") . "&gt;</nobr></b>");
 	} elseif (empty($gallery->app->emailGreeting)) {
 		return sprintf($default,
-			$gallery->app->galleryTitle,
-			$gallery->app->photoAlbumURL,
-			$gallery->app->galleryTitle);
+		$gallery->app->galleryTitle,
+		$gallery->app->photoAlbumURL,
+		$gallery->app->galleryTitle);
 	} else {
 		return $gallery->app->emailGreeting;
 	}
@@ -185,7 +187,6 @@ Gallery @ %s Administrator.");
 }
 
 function welcomeMsgPlaceholderList() {
-
 	$placeholders = array(
 		'galleryurl' => _("The Url to your Gallery."),
 		'gallerytitle' => _("Title of your Gallery."),
@@ -205,6 +206,7 @@ function welcomeMsgPlaceholderList() {
  */
 function resolveWelcomeMsg($placeholders = array()) {
 	global $gallery;
+
 	$welcomeMsg =  welcome_email();
 
 	$placeholders['galleryurl'] = $gallery->app->photoAlbumURL;
@@ -213,7 +215,7 @@ function resolveWelcomeMsg($placeholders = array()) {
 
 	foreach (welcomeMsgPlaceholderList() as $key => $trash) {
 		$welcomeMsg = str_replace('!!'. strtoupper($key) .'!!',
-			isset($placeholders[$key]) ? $placeholders[$key] : '', $welcomeMsg);
+		isset($placeholders[$key]) ? $placeholders[$key] : '', $welcomeMsg);
 	}
 
 	return $welcomeMsg;
@@ -234,42 +236,43 @@ function emailComments($id, $comment_text, $commenter_name) {
 	$text = '';
 
 	if (!empty($to)) {
-	    $text .= '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">';
-	    $text .= "\n\n<html>";
-	    $text .= "\n  <head>";
-	    $text .= "\n  <title>$subject</title>";
-	    $text .= "\n  </head>\n<body>\n<p>";
-	    $text .= sprintf(_("A new comment has been added to Gallery: %s"), $gallery->app->galleryTitle);
-	    $text .= "\n</p>";
-	    $text .= sprintf(_("The comment was added by %s to this %s in this %s."),
-			$commenter_name,
-			'<a href="'. makeAlbumHeaderUrl($gallery->session->albumName, $id) .'">'. _("Item") .'</a>',
-			'<a href="'. makeAlbumHeaderUrl($gallery->session->albumName) .'">'. _("Album") .'</a>');
-	    $text .= "\n<br>". _("*** Begin comment ***") ."<br>\n";
-	    $text .= nl2br($comment_text);
-	    $text .= "<br>\n". _("***End comment ***") . "\n<p>\n";
-	    $text .= _("If you no longer wish to receive emails about this image, follow the links above and ensure that 'Email me when comments are added' is unchecked in both the photo and album page (You'll need to login first).");
-	    $text .= "\n</p>\n</body>\n</html>";
+		$text .= '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">';
+		$text .= "\n\n<html>";
+		$text .= "\n  <head>";
+		$text .= "\n  <title>$subject</title>";
+		$text .= "\n  </head>\n<body>\n<p>";
+		$text .= sprintf(_("A new comment has been added to Gallery: %s"), $gallery->app->galleryTitle);
+		$text .= "\n</p>";
+		$text .= sprintf(_("The comment was added by %s to this %s in this %s."),
+		$commenter_name,
+		'<a href="'. makeAlbumHeaderUrl($gallery->session->albumName, $id) .'">'. _("Item") .'</a>',
+		'<a href="'. makeAlbumHeaderUrl($gallery->session->albumName) .'">'. _("Album") .'</a>');
+		$text .= "\n<br>". _("*** Begin comment ***") ."<br>\n";
+		$text .= nl2br($comment_text);
+		$text .= "<br>\n". _("***End comment ***") . "\n<p>\n";
+		$text .= _("If you no longer wish to receive emails about this image, follow the links above and ensure that 'Email me when comments are added' is unchecked in both the photo and album page (You'll need to login first).");
+		$text .= "\n</p>\n</body>\n</html>";
 
-        $logmsg = sprintf(_("New comment for %s."), makeAlbumHeaderUrl($gallery->session->albumName, $id));
+		$logmsg = sprintf(_("New comment for %s."), makeAlbumHeaderUrl($gallery->session->albumName, $id));
 
-	    gallery_mail($to, $subject, $text, $logmsg, true, NULL, false, true);
+		gallery_mail($to, $subject, $text, $logmsg, true, NULL, false, true);
 	}
 }
 
 function emailLogMessage($logmsg, $result, $isNotifyMail) {
 	global $gallery;
+
 	if (!$result) {
 		$logmsg = sprintf(_("FAILED") ." / FAILED: %s", $logmsg);
 	}
 	if (isset($gallery->app->email_notification) &&
-	  in_array("logfile", $gallery->app->email_notification)) {
+	in_array("logfile", $gallery->app->email_notification)) {
 		$logfile = $gallery->app->userDir."/email.log";
 		logMessage($logmsg, $logfile);
 	}
 
 	if (isset($gallery->app->email_notification) &&
-	  in_array("email", $gallery->app->email_notification)) {
+	in_array("email", $gallery->app->email_notification)) {
 		$subject = _("Email activity");
 		if ($subject != "Email activity") {
 			$subject .= "/Email activity";
@@ -278,12 +281,12 @@ function emailLogMessage($logmsg, $result, $isNotifyMail) {
 		$subject = unhtmlentities($subject);
 
 		gallery_mail($gallery->app->adminEmail,
-			$subject,
-			$logmsg . emailDisclaimer(),
-			'',
-			false,
-			$gallery->app->senderEmail,
-			true
+		$subject,
+		$logmsg . emailDisclaimer(),
+		'',
+		false,
+		$gallery->app->senderEmail,
+		true
 		);
 	}
 }
