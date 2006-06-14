943a4cf797bcb4e84453cb07b43dca81d695b784
Jens Alexander Tkotz <jens@peino.de>
2006-06-14 12:48:40 +0000

    2006-06-14 Jens Tkotz <jens@peino.de> 1.6-svn-b1-j52-UNSTABLE
     *** CHANGE ***
    
     Remove of html_wrap/gallery.photo.default and
    html_wrap/photo.footer.default
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/DEV_1_6@13821 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -22,7 +22,7 @@
 ?>
 <?php
 
-$gallery->version = '1.6-svn-b1-j51-UNSTABLE';
+$gallery->version = '1.6-svn-b1-j52-UNSTABLE';
 $gallery->config_version = 97;
 $gallery->album_version = 38;
 $gallery->user_version = 6;
