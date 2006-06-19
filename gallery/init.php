78fb8dfab45ad2ecf0989581d4f269920fe6326e
Jens Alexander Tkotz <jens@peino.de>
2006-06-19 13:50:22 +0000

    2006-06-19 Jens Tkotz <jens@peino.de> 1.6-svn-b1-j61-UNSTABLE
     * Fix: init_language() is not called redundant in init.php
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/DEV_1_6@13852 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/init.php
+++ gallery/init.php
@@ -182,12 +182,9 @@ if (!isset($GALLERY_NO_SESSIONS)) {
 
 $gallerySanity = gallerySanityCheck();
 
-// Languages need to be initialized early or installations without gettext will break.
-// initLanguage is called again later in init.php to pick up user settings.
-initLanguage();
-
 /* Make sure that Gallery is set up properly */
 if ($gallerySanity != NULL) {
+    initLanguage();
     include_once(dirname(__FILE__) . "/includes/errors/$gallerySanity");
     exit;
 }
