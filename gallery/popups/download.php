0f60aef19c9d96ce9553a2c8c5633227649dfb0e
Jens Alexander Tkotz <jens@peino.de>
2006-07-10 22:35:40 +0000

    2006-07-11 Jens Tkotz <jens@peino.de> 1.6-svn-b1-j81-UNSTABLE
     * FIX: Headers were sent after the header were sent ;) in download.php
       Thanks to Kai.
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/DEV_1_6@13946 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/popups/download.php
+++ gallery/popups/download.php
@@ -24,10 +24,9 @@
 
 require_once(dirname(dirname(__FILE__)) . '/init.php');
 
-list($doit, $full) =
-getRequestVar(array('doit', 'full'));
+list($doit, $full) = getRequestVar(array('doit', 'full'));
 
-printPopupStart(gTranslate('core', "Download album as archive"));
+$message = array();
 
 if (!empty($doit)) {
     $albumItemNames = $gallery->album->getAlbumItemNames($gallery->user, $full, false, true);
@@ -39,51 +38,59 @@ if (!empty($doit)) {
                 downloadFile($zipfileName);
             }
             else {
-                echo gallery_error('<br>'. gTranslate('core', "Zipdownload would work, but is disabled when debugging."));
+                $message = array(array(
+                    'type' => 'information',
+                    'text' => gTranslate('core', "Zipdownload would work, but is disabled when debugging.")));
             }
         }
     }
 }
+
+printPopupStart(gTranslate('core', "Download album as archive"));
+
+echo infoBox($message);
+
+list($numItems, $numAlbums, $numPhotos) = $gallery->album->numItems($gallery->user, true);
+
+$albumSize = $gallery->album->getAlbumSize($gallery->user, $full, false, true);
+
+echo "\n<p class=\"g-title g-emphasis\">";
+
+if ($gallery->album->numPhotos(1)) {
+    echo $gallery->album->getHighlightTag();
+    echo "<br>";
+}
+echo $gallery->album->fields["title"];
+echo "</p>";
+
+$textNumItems = sprintf(gTranslate('core', "This album contains just one item in total.", "This album contains %d items in total.", $numItems), $numItems);
+$textNumSubAlbums = sprintf(gTranslate('core', "One subalbum", "%d subalbums", $numAlbums, gTranslate('core', "No subalbums")), $numAlbums);
+$textNumPhotos = sprintf(gTranslate('core', "one photo/movie", "%d photos/movies", $numPhotos , gTranslate('core', "no photo/movie")), $numPhotos);
+
+printf("%s ". gTranslate('core', "%s and %s."), $textNumItems, $textNumSubAlbums, $textNumPhotos);
+
+if($numPhotos > 0) {
+    echo '<p>'. sprintf(gTranslate('core', "Approximated size of zipfile: %s"), formatted_filesize($albumSize)) .'</p>';
+
+    echo makeFormIntro('download.php', array(), array('type' => 'popup'));
+    ?>
+        <input type="radio" id="full" name="full" value="1" <?php echo ($full ? ' checked' : '') ?> onChange="document.g1_form.submit()">
+        <label for="full"><?php echo gTranslate('core', "Full Version"); ?></label>
+        <br>
+        <input type="radio" id="resized" name="full" value="0" <?php echo (!$full ? ' checked' : '') ?> onChange="document.g1_form.submit()">
+        <label for="resized"><?php echo gTranslate('core', "Resized Version"); ?></label>
+
+        <br><br>
+    <?php
+    echo gSubmit('doit', gTranslate('core', "_Download"));
+    echo gButton('cancel', gTranslate('core', "_Cancel"), 'parent.close()');
+    echo "\n</form>";
+}
 else {
-    list($numItems, $numAlbums, $numPhotos) = $gallery->album->numItems($gallery->user, true);
-
-    $albumSize = $gallery->album->getAlbumSize($gallery->user, $full, false, true);
-
-    echo "\n<p class=\"g-title g-emphasis\">";
-
-    if ($gallery->album->numPhotos(1)) {
-        echo $gallery->album->getHighlightTag();
-        echo "<br>";
-    }
-    echo $gallery->album->fields["title"];
-    echo "</p>";
-
-    $textNumItems = sprintf(gTranslate('core', "This album contains just one item in total.", "This album contains %d items in total.", $numItems), $numItems);
-    $textNumSubAlbums = sprintf(gTranslate('core', "One subalbum", "%d subalbums", $numAlbums, gTranslate('core', "No subalbums")), $numAlbums);
-    $textNumPhotos = sprintf(gTranslate('core', "one photo/movie", "%d photos/movies", $numPhotos , gTranslate('core', "no photo/movie")), $numPhotos);
-
-    echo sprintf("%s ". gTranslate('core', "%s and %s."), $textNumItems, $textNumSubAlbums, $textNumPhotos);
-
-    if($numPhotos > 0) {
-        echo '<p>'. sprintf(gTranslate('core', "Approximated size of zipfile: %s"), formatted_filesize($albumSize)) .'</p>';
-
-        echo makeFormIntro('download.php',
-          array('onChange' => 'document.g1_form.submit()'),
-          array('type' => 'popup'));
-
-        echo "\n". '<input type="radio" name="full" value="1"'. ($full ? ' checked' : '') .'>'. gTranslate('core', "Full Version") .'<br>';
-        echo "\n". '<input type="radio" name="full" value="0"'. (!$full ? ' checked' : '') .'>'. gTranslate('core', "Resized Version") .'<br>';
-        echo "\n<br>";
-        echo "\n". '<input type="submit" name="doit" value="'. gTranslate('core', "Download") .'" class="g-button">';
-        echo "\n". '<input type="button" value="'. gTranslate('core', "Cancel") .'" onclick="parent.close()" class="g-button">';
-        echo "\n</form>";
-    }
-    else {
-	echo "<br><br>";
-	echo gTranslate('core', "This album is not empty, but contains no photo or movie! Download wouldn't make sense.");
-	echo "<br><br>";
-        echo "\n". '<input type="button" value="'. gTranslate('core', "Close Window") .'" onclick="parent.close()" class="g-button">';
-    }
+    echo "<br><br>";
+    echo gTranslate('core', "This album is not empty, but contains no photo or movie! Download wouldn't make sense.");
+    echo "<br><br>";
+    echo gButton('close', gTranslate('core', "_Close Window"),'parent.close()');
 }
 ?>
 </div>
