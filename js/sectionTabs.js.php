7a61ebda81763e649e13072dec310372f8ba2e35
Jens Alexander Tkotz <jens@peino.de>
2006-09-08 14:51:37 +0000

    Just indenting.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@14474 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- js/sectionTabs.js.php
+++ js/sectionTabs.js.php
@@ -7,57 +7,56 @@
 
 function insertSectionToggle() {
 ?>
+    <!-- This Javascript and the Tabs are inspired by the Horde Forms code -->
 
-<!-- This Javascript and the Tabs are inspired by the Horde Forms code -->
+    function configSection(inittab) {
 
-        function configSection(inittab) {
+        this.toggle = function(id) {
+            document.getElementById(this.oldtab).style.display 	= 'none';
+            document.getElementById('tab_' + this.oldtab).className = '';
+            document.getElementById(id).style.display 		= 'inline';
+            document.getElementById('tab_' + id).className 		= 'g-activeTab';
 
-                this.toggle = function(id) {
-                        document.getElementById(this.oldtab).style.display 	= 'none';
-                        document.getElementById('tab_' + this.oldtab).className = '';
-                        document.getElementById(id).style.display 		= 'inline';
-                        document.getElementById('tab_' + id).className 		= 'g-activeTab';
+            this.oldtab = id;
+            document.getElementById('initialtab').value = id;
+            this.currentSectionNr= this.getTabByName(id);
+        }
 
-                        this.oldtab=id;
-			document.getElementById('initialtab').value = id;
-			this.currentSectionNr= this.getTabByName(id);
+        this.getTabByNr = function(nr) {
+            for (var itemNr=0; itemNr <= Sections.length; itemNr++) {
+                if (Sections[itemNr] == Sections[nr]) {
+                    return (Sections[itemNr]);
                 }
+            }
+        }
 
-		this.getTabByNr = function(nr) {
-			for (var itemNr=0; itemNr <= Sections.length; itemNr++) {
-				if (Sections[itemNr] == Sections[nr]) {
-					return (Sections[itemNr]);
-				}
-			}
-		}
+        this.getTabByName = function(name) {
+            for (var itemNr = 0; itemNr <= Sections.length; itemNr++) {
+                if (Sections[itemNr] == name) {
+                    return (itemNr);
+                }
+            }
+        }
 
-		this.getTabByName = function(name) {
-			for (var itemNr=0; itemNr <= Sections.length; itemNr++) {
-				if (Sections[itemNr] == name) {
-					return (itemNr);
-				}
-			}
-		}
+        this.nextTab = function() {
+            if (this.currentSectionNr < Sections.length-1) {
+                nextTab = this.getTabByNr(this.currentSectionNr+1);
+                this.toggle(nextTab);
+            }
+        }
 
-		this.nextTab = function() {
-			if (this.currentSectionNr < Sections.length-1) {
-				nextTab=this.getTabByNr(this.currentSectionNr+1);
-				this.toggle(nextTab);
-			}
-		}
+        this.prevTab = function() {
+            if (this.currentSectionNr >0) {
+                prevTab = this.getTabByNr(this.currentSectionNr-1);
+                this.toggle(prevTab);
+            }
+        }
 
-		this.prevTab = function() {
-			if (this.currentSectionNr >0) {
-				prevTab=this.getTabByNr(this.currentSectionNr-1);
-				this.toggle(prevTab);
-			}
-		}
+        // Init Values
 
-		// Init Values
+        this.oldtab = inittab;
+        this.currentSectionNr = this.getTabByName(inittab);
 
-                this.oldtab=inittab;
-		this.currentSectionNr= this.getTabByName(inittab);
-
-	}
-<?php
+    }
+    <?php
 }
