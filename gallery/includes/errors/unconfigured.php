1ad4b905ca79f5dfa0eb5b2adc3082210807427f
Jens Alexander Tkotz <jens@peino.de>
2006-09-09 19:13:40 +0000

    2006-09-09 Jens Tkotz <jens@peino.de> 1.5.5-svn-b7
      * Change: Enhanced wording in config.
    
      * Fix: Little CSS for for .g-littlered.
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_5_LEGACY@14491 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/includes/errors/unconfigured.php
+++ gallery/includes/errors/unconfigured.php
@@ -1,7 +1,7 @@
 <?php
 // $Id$
 ?>
-<?php 
+<?php
     doctype();
 ?>
 <html>
@@ -15,18 +15,18 @@
 <p class="header"><?php echo _("Gallery has not been configured!") ?></p>
 
 <p class="sitedesc">
-<?php 
+<?php
     echo _("Gallery must be configured before you can use it.");
 ?>
 </p>
-<table class="sitedesc">
+<table class="sitedesc" style="text-align:left">
 <tr>
 	<td><?php echo _("1."); ?></td>
-	<td><?php echo _("Create an empty file .htaccess and an empty file config.php"); ?></td>
+	<td><?php echo _("Create an empty file .htaccess and an empty file config.php in your Gallery folder."); ?></td>
 </tr>
 <tr>
 	<td><?php echo _("2."); ?></td>
-	<td><?php echo _("Create an albums folder for your pictures and movies."); ?></td>
+	<td><?php echo _("Create an albums folder for your pictures and movies. This folder can be anywhere in your webspace."); ?></td>
 </tr>
 <tr>
 	<td colspan="2" class="emphasis"><?php echo _("Make sure that both files and the folder are read and writeable for your webserver !"); ?></td>
@@ -34,15 +34,17 @@
 </table>
 
 <p>
-<?php 
-    echo sprintf(_("Then start the %sConfiguration Wizard%s."), 
-	'<a href="'. makeGalleryUrl('setup/index.php') .'">', '</a>'); 
+<?php
+    echo sprintf(_("Then start the %sConfiguration Wizard%s."),
+	'<a href="'. makeGalleryUrl('setup/index.php') .'">', '</a>');
     echo '<br>';
     include(dirname(__FILE__) . "/configure_help.php");
 ?>
 </p>
 </div>
-<?php 
+<?php
+    echo sprintf(_("%sNote:%s When you get an 'error 500' when accessing the config wizard, try removing the .htaccess file the setup folder."), '<b>', '</b>');
+    echo "\n<br>";
     echo gallery_validation_link('index.php', true);
 ?>
 </body>
