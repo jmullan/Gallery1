d78b5e1a4387b5bb202dae50edec38e1eb5a22fe
Jens Alexander Tkotz <jens@peino.de>
2006-09-10 22:13:31 +0000

    Just indenting.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@14500 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/valchecks.php
+++ lib/valchecks.php
@@ -1,24 +1,24 @@
 <?php
 /*
- * Gallery - a web based photo album viewer and editor
- * Copyright (C) 2000-2006 Bharat Mediratta
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * $Id$
- */
+* Gallery - a web based photo album viewer and editor
+* Copyright (C) 2000-2006 Bharat Mediratta
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or (at
+* your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+*
+* $Id$
+*/
 ?>
 <?php
 
@@ -78,7 +78,8 @@ function isValidInteger($mixed, $includingZero = false, $default = NULL, $emptyA
     if (! is_numeric($mixed)) {
         if (isset($default)) {
             return array(1,$default, gTranslate('common', "Value was set to given default. Because the original value is not numeric."));
-        } else {
+        }
+        else {
             return array(2, false, gTranslate('common', "The given Value is not numeric."));
         }
     }
@@ -86,7 +87,8 @@ function isValidInteger($mixed, $includingZero = false, $default = NULL, $emptyA
     if($mixed < $minimum) {
         if (isset($default)) {
             return array(1, $default, gTranslate('common', "Value was set to given default. Because the original value is not a valid Integer"));
-        } else {
+        }
+        else {
             return array(2, false, gTranslate('common', "The given Value not a valid Integer."));
         }
     }
