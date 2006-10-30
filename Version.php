248d3505dd7303f495f8ae2beea8a6463676d837
Jens Alexander Tkotz <jens@peino.de>
2006-10-30 01:51:21 +0000

    2006-10-30 Jens Tkotz <jens@peino.de> 1.6-alpha2-svn-b22
     * Change: Added backround images for header and adminbar.
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@14999 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -22,9 +22,9 @@
 ?>
 <?php
 
-$gallery->version = '1.6-alpha2-svn-b21';
+$gallery->version = '1.6-alpha2-svn-b22';
 $gallery->config_version = 99;
-$gallery->album_version = 38;
+$gallery->album_version = 39;
 $gallery->user_version = 6;
 $gallery->url = "http://gallery.sourceforge.net";
 
