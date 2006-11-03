dd44aaff54528b55011890437f52ba00ae56da6e
Jens Alexander Tkotz <jens@peino.de>
2006-11-03 04:30:14 +0000

    2006-11-03 Jens Tkotz <jens@peino.de> 1.6-alpha3-svn-b7
     * Fix: Show descriptionbix in photo view only if there ,
            *is* a description.
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@15045 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- Version.php
+++ Version.php
@@ -22,7 +22,7 @@
 ?>
 <?php
 
-$gallery->version = '1.6-alpha3-svn-b6';
+$gallery->version = '1.6-alpha3-svn-b7';
 $gallery->config_version = 99;
 $gallery->album_version = 39;
 $gallery->user_version = 6;
