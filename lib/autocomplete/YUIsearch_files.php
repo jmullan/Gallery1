6573acac15e3ebc66fe6108edebcaad155070665
Jens Alexander Tkotz <jens@peino.de>
2007-02-20 19:05:25 +0000

    Indenting.
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@15839 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- lib/autocomplete/YUIsearch_files.php
+++ lib/autocomplete/YUIsearch_files.php
@@ -1,29 +1,29 @@
 <?php
 /*
- * Gallery - a web based photo album viewer and editor
- * Copyright (C) 2000-2007 Bharat Mediratta
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or (at
- * your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
- *
- * $Id: YUIsearch_files.php 12505 2006-01-08 20:15:14Z jenst $
+* Gallery - a web based photo album viewer and editor
+* Copyright (C) 2000-2007 Bharat Mediratta
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or (at
+* your option) any later version.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
+*
+* $Id: YUIsearch_files.php 12505 2006-01-08 20:15:14Z jenst $
 */
 
 /**
  * @package	yui
  * @author	Yahoo!
- * @author	Jens Tkotz <jens@peino.de>
+ * @author	Jens Tkotz
  */
 ?>
 <?php
@@ -35,10 +35,10 @@ setGalleryPaths();
 
 if (getOS() == OS_WINDOWS) {
 	require(GALLERY_BASE . '/platform/fs_win32.php');
-} else {
+}
+else {
 	require(GALLERY_BASE . '/platform/fs_unix.php');
 }
-
 
 $query = $_GET['query'];
 $results = search($query);
@@ -81,7 +81,8 @@ function search($query) {
 
 			if (in_array($file, $forbidden) ||
 				fs_fileIsHidden($file) ||
-				(!acceptableFormat($ext) && !acceptableArchive($ext) && !fs_is_dir($path))) {
+				(!acceptableFormat($ext) && !acceptableArchive($ext) && !fs_is_dir($path)))
+			{
 				continue;
 			}
 			else {
