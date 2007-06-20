87d0bc22d70da00a0bb6063af8c27895e958f25a
Jens Alexander Tkotz <jens@peino.de>
2007-06-20 00:40:12 +0000

    2007-06-20 Jens Tkotz <jens@peino.de> 1.6-RC1-svn-b74
     * Chnage: Use of navigation via arrow keys in photo view.
    
     * Fix: Remove some debug code.
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/trunk/gallery@16668 57fcd75e-5312-0410-8df3-f5eb6fbb1595

---
+++ js/arrowkey_nav.js
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+/**
+ * Gallery SVN ID:
+ * $Id$
+*/
+
+	function cursor_nav(e) {
+		if(document.all) {
+			taste = window.event.keyCode;
+		}
+		else {
+			taste = e.which;
+		}
+		switch (taste) {
+			case 37:
+				backward = document.getElementById("g-nav-backward_1");
+				window.location.href = backward.href;
+			break;
+
+			case 39:
+				forward = document.getElementById("g-nav-forward_1");
+				window.location.href = forward.href;
+			break;
+
+			default:
+			break;
+		}
+	}
+
+	document.onkeydown = cursor_nav;
+</script>
