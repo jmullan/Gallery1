17d6a85066b00b966c4a3a62c8082a11eef06d73
Jens Alexander Tkotz <jens@peino.de>
2008-08-06 13:12:39 +0000

    2008-08-06 Jens Tkotz <jens@peino.de> 1.5.8-svn-b32
     *
     *** SECURITY BUGFIX ****
    
     + Inadequate Password Complexity Enforcement +
    
     The application does not enforce adequate password complexity
     requirements for user passwords.
    
     + New Password Functionality Susceptible to Brute Force Attacks +
    
    
    
    git-svn-id: file:///home/jmullan/src/gallery-git/repo/branches/BRANCH_1_5_LEGACY@17805 57fcd75e-5312-0410-8df3-f5eb6fbb1595

--- gallery/Version.php
+++ gallery/Version.php
@@ -22,7 +22,7 @@
 ?>
 <?php
 
-$gallery->version = '1.5.8-svn-b31';
+$gallery->version = '1.5.8-svn-b32';
 $gallery->config_version = 96;
 $gallery->album_version = 37;
 $gallery->user_version = 6;
--- gallery/new_password.php
+++ gallery/new_password.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Gallery - a web based photo album viewer and editor
- * Copyright (C) 2000-2007 Bharat Mediratta
+ * Copyright (C) 2000-2008 Bharat Mediratta
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,113 +28,114 @@ list($hash, $uname, $save, $new_password1, $new_password2) =
 list($fullname, $email, $defaultLanguage) =
 	getRequestVar(array('fullname', 'email', 'defaultLanguage'));
 
-$error_string = '';
-
-if (!isset($hash)) {
-	   	$error_string .= gTranslate('core', "missing hash parameter") . "<br>";
-}
-if (empty($uname) ) {
-	   	$error_string .= gTranslate('core', "Not a valid username") . "<br>";
-} else {
-       	$tmpUser = $gallery->userDB->getUserByUsername($uname);
-       	if (empty($tmpUser)) {
-		   	$error_string .= gTranslate('core', "Not a valid username") . "<br>";
-       	} else if (!$tmpUser->checkRecoverPasswordHash($hash)) {
-		   	$error_string .= gTranslate('core', "The recovery password is not the expected value, please try again") . "<br>";
-	}
-}
-
 $errorCount = 0;
-if (!empty($save)) {
-	if (empty($new_password1) ) {
-		   	$gErrors["new_password1"] = gTranslate('core', "You must provide your new password.");
-	       	$errorCount++;
-	} else if (strcmp($new_password1, $new_password2)) {
-		   	$gErrors["new_password2"] = gTranslate('core', "Passwords do not match!");
-	       	$errorCount++;
-       	} else {
-	       	$gErrors["new_password1"] = $gallery->userDB->validPassword($new_password1);
-	       	if ($gErrors["new_password1"]) {
-		       	$errorCount++;
-	       	}
-       	}
 
-	// security check;
-	if($fullname != strip_tags($fullname)) {
-            $gErrors["fullname"] =
-				sprintf(gTranslate('core', "%s contained invalid data, resetting input."), htmlentities($fullname));
-            $errorCount++;
-        }
+printPopupStart(gTranslate('core', "Make New Password"));
 
-	if (!empty($email) && !check_email($email)) {
-				$gErrors['email'] = gTranslate('core', "You must specify a valid email address.");
-                $errorCount++;
-        }
-
-       	if (!$error_string && !$errorCount) {
-       	    $tmpUser->setFullname($fullname);
-       	    $tmpUser->setEmail($email);
-       	    if (isset($defaultLanguage)) {
-       	        $tmpUser->setDefaultLanguage($defaultLanguage);
-       	        $gallery->session->language=$defaultLanguage;
-       	    }
-       	    if ($new_password1) {
-       	        $tmpUser->setPassword($new_password1);
-       	    }
-       	    $tmpUser->genRecoverPasswordHash(true);
-       	    $tmpUser->log("new_password_set");
-       	    $tmpUser->save();
-
-       	    // Switch over to the new username in the session
-       	    $gallery->session->username = $uname;
-       	    header("Location: " . makeAlbumHeaderUrl());
-       	}
-}
-
-$allowChange["uname"] = false;
-$allowChange["email"] = true;
-$allowChange["fullname"] = true;
-$allowChange["password"] = true;
-$allowChange["old_password"] = false;
-$allowChange["send_email"] = false;
-$allowChange["member_file"] = false;
-
-doctype();
-?>
-<html>
-<head>
-  <title><?php echo _("Make New Password") ?></title>
-  <?php common_header(); ?>
-</head>
-<body dir="<?php echo $gallery->direction ?>" class="popupbody">
-<div class="popuphead"><?php echo _("Make New Password") ?></div>
-<div class="popup" align="center">
-<?php 
-if ($error_string) {
-       	echo gallery_error($error_string);
-       	echo "<a href='albums.php'>" . _("Enter the Gallery") . "</a></div></body></html>"; 
+if (empty($hash) || empty($uname)) {
+	showInvalidReqMesg();
 	exit;
 }
 
-echo _("You can change your user information here.");
-echo _("You must enter the new password twice.");
+if (!empty($uname) ) {
+	$tmpUser = $gallery->userDB->getUserByUsername($uname);
+	if (! empty($tmpUser) && !$tmpUser->checkRecoverPasswordHash($hash)) {
+		if($tmpUser->lastAction ==  "new_password_request") {
+			showInvalidReqMesg(gTranslate('core', "The recovery password is not the expected value, please try again."));
+			exit;
+		}
+		else {
+			showInvalidReqMesg();
+			exit;
+		}
+	}
+	else {
+		showInvalidReqMesg();
+		exit;
+	}
+}
 
-echo "\<p>";
+if (!empty($save)) {
+	$saveOk = true;
 
-echo makeFormIntro('new_password.php', array('name' => 'usermodify_form'));
-$fullname = $tmpUser->getFullname();
-$email = $tmpUser->getEmail();
+	if (empty($new_password1) ) {
+		$gErrors['new_password1'] = gTranslate('core', "You must provide your new password.");
+		$saveOk = false;
+	}
+	else if (strcmp($new_password1, $new_password2)) {
+		$gErrors['new_password2'] = gTranslate('core', "Passwords did not match!");
+		$errorCount++;
+	}
+	else {
+		$gErrors['new_password1'] = $gallery->userDB->validPassword($new_password1);
+		if ($gErrors['new_password1']) {
+			$saveOk = false;
+		}
+	}
+
+	// security check;
+	if(! isXSSclean($fullname)) {
+		$gErrors['fullname'] =
+			sprintf(gTranslate('core', "%s contained invalid data, sanitizing input."),
+					sanitizeInput($fullname));
+		$saveOk = false;
+	}
+
+	if (!empty($email) && !check_email($email)) {
+		$gErrors['email'] = gTranslate('core', "You must specify a valid email address.");
+		$saveOk = false;
+	}
+
+	if ($saveOk) {
+		$tmpUser->setFullname($fullname);
+		$tmpUser->setEmail($email);
+
+		if (isset($defaultLanguage)) {
+			$tmpUser->setDefaultLanguage($defaultLanguage);
+			$gallery->session->language=$defaultLanguage;
+		}
+
+		if ($new_password1) {
+			$tmpUser->setPassword($new_password1);
+		}
+
+		$tmpUser->genRecoverPasswordHash(true);
+		$tmpUser->log("new_password_set");
+		$tmpUser->save();
+
+		// Switch over to the new username in the session
+		$gallery->session->username = $uname;
+		header("Location: " . makeAlbumHeaderUrl());
+	}
+}
+
+$allowChange['uname']		= false;
+$allowChange['email']		= true;
+$allowChange['fullname']	= true;
+$allowChange['password']	= true;
+$allowChange['old_password']	= false;
+$allowChange['send_email']	= false;
+$allowChange['member_file']	= false;
+
+$fullname	 = $tmpUser->getFullname();
+$email		 = $tmpUser->getEmail();
 $defaultLanguage = $tmpUser->getDefaultLanguage();
 
-echo "\n<p>";
+echo '<div class="g-sitedesc">';
+echo gTranslate('core', "You can change your user information here.");
+echo gTranslate('core', "You must enter the new password twice.");
+
+echo "\n</div>";
+
+echo makeFormIntro('new_password.php', array('action' => '#','name' => 'usermodify_form', 'style' => 'padding: 15px 50px 0 50px '));
 
 include(dirname(__FILE__) . '/html/userData.inc');
 
 ?>
 <p>
 <input type="hidden" name="hash" value="<?php echo $hash ?>">
-<input type="submit" name="save" value="<?php echo _("Save") ?>">
-<input type="button" name="cancel" value="<?php echo _("Cancel") ?>" onclick="location.href='<?php echo $gallery->app->photoAlbumURL; ?>'">
+<?php echo gSubmit('save', gTranslate('core', "Save")); ?>
+<?php echo gButton('cancel', gTranslate('core', "Cancel"), "location.href='". $gallery->app->photoAlbumURL ."'"); ?>
 </form>
 
 <script language="javascript1.2" type="text/JavaScript">
